98bee8304526f744b46cf9ee293ab1b42bda4ef7
Par Wenaker <par.wenaker@callistaenterprise.se>
2013-08-23 13:02:48 +0200

    Made refactoring (cherry picked from commit 22bbb2b5b9fbb268a2d161f0967622576b05de9b)
    Signed-off-by: Par Wenaker <par.wenaker@callistaenterprise.se>

--- web/src/main/java/se/inera/certificate/integration/RegistreraIntygResponder.java
+++ web/src/main/java/se/inera/certificate/integration/RegistreraIntygResponder.java
@@ -1,5 +1,6 @@
 package se.inera.certificate.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Throwables;
 import intyg.registreraintyg._1.RegistreraIntygResponderInterface;
 import org.apache.commons.io.IOUtils;
@@ -27,7 +28,11 @@ public class RegistreraIntygResponder implements RegistreraIntygResponderInterfa
 
     private static final int BAD_REQUEST = 400;
 
+    private static final int NOT_FOUND = 404;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistreraIntygResponder.class);
+
+    private static ObjectMapper objectMapper = new CustomObjectMapper();
 
     @Autowired
     private ModuleRestApiFactory moduleRestApiFactory;
@@ -37,17 +42,16 @@ public class RegistreraIntygResponder implements RegistreraIntygResponderInterfa
 
     @Override
     public void registreraIntyg(Holder<se.inera.certificate.common.v1.Utlatande> utlatande) {
+        // Get type of intyg
         String type = utlatande.value.getTypAvUtlatande().getCode();
 
+        // Get module external format from the module
         String externalJson = unmarshall(type, utlatande.value);
 
-        Utlatande commonUtlatande = null;
-        try {
-            commonUtlatande = new CustomObjectMapper().readValue(externalJson, Utlatande.class);
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        // Map common parts of the module external format to the common format.
+        Utlatande commonUtlatande = convertToCommonUtlatande(externalJson);
 
+        // Store the intyg and meta data in a Certificate object
         certificateService.storeCertificate(commonUtlatande, externalJson);
     }
 
@@ -57,51 +61,38 @@ public class RegistreraIntygResponder implements RegistreraIntygResponderInterfa
 
         Response response = endpoint.unmarshall(utlatande);
 
+        String entityContent = readEntity(response);
+
         switch (response.getStatus()) {
+            case NOT_FOUND:
+                throw new ValidationException("Module of type "+ type +" not found, 404!");
             case BAD_REQUEST:
-                try {
-                    InputStream inputStream = (InputStream) response.getEntity();
-                    String validationErrorMessage = IOUtils.toString(inputStream);
-                    throw new ValidationException(validationErrorMessage);
-                } catch (IOException e) {
-                    throw new RuntimeException("Failed to read response for validation of '" + type + "' certificate.", e);
-                }
+                throw new ValidationException(entityContent);
             case OK:
-                InputStream inputStream = (InputStream) response.getEntity();
-                try {
-                    String json = IOUtils.toString(inputStream);
-                    return json;
-                } catch (IOException e) {
-                    Throwables.propagate(e);
-                }
-                break;
+                return entityContent;
         }
         String errorMessage = "Failed to validate certificate for certificate type '" + type + "'. HTTP status code is " + response.getStatus();
         LOGGER.error(errorMessage);
         throw new ValidationException(errorMessage);
     }
 
-    private void validate(String type, Utlatande utlatande) {
-
-        ModuleRestApi endpoint = moduleRestApiFactory.getModuleRestService(type);
-
-        Response response = endpoint.validate(utlatande);
-
-        switch (response.getStatus()) {
-            case BAD_REQUEST:
-                try {
-                    InputStream inputStream = (InputStream) response.getEntity();
-                    String validationErrorMessage = IOUtils.toString(inputStream);
-                    throw new ValidationException(validationErrorMessage);
-                } catch (IOException e) {
-                    throw new RuntimeException("Failed to read response for validation of '" + type + "' certificate.", e);
-                }
-            case OK:
-                break;
-            default:
-                String errorMessage = "Failed to validate certificate for certificate type '" + type + "'. HTTP status code is " + response.getStatus();
-                LOGGER.error(errorMessage);
-                throw new ValidationException(errorMessage);
+    private String readEntity(Response response) {
+        try {
+            if (response.hasEntity()) {
+                return IOUtils.toString((InputStream) response.getEntity());
+            }
+            return null;
+        } catch (IOException ioe) {
+            throw Throwables.propagate(ioe);
         }
     }
+
+    private Utlatande convertToCommonUtlatande(String externalJson) {
+        try {
+            return objectMapper.readValue(externalJson, Utlatande.class);
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
 }
