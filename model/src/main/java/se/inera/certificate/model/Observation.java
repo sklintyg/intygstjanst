ec0b114b2c65c835c987b2c1256b16fb51511922
erik forsgren <erik.forsgren@r2m.se>
2013-08-29 13:07:05 +0200

    Updated External model to match the domain model
    - Added Observationssamband
    - Added Bestallare
    - Added Betalningsmottagare
    - Added Omfattning
    - Added PatientRelation
    - Added Rekommendation
    - Added Substansintag
    - Added Utforarroll

--- model/src/main/java/se/inera/certificate/model/Observation.java
+++ model/src/main/java/se/inera/certificate/model/Observation.java
@@ -1,64 +1,120 @@
 package se.inera.certificate.model;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.joda.time.LocalDateTime;
 
 /**
  * @author andreaskaltenbach
  */
 public class Observation {
 
-    private Kod observationsKategori;
-    private Kod observationsKod;
-    private PartialInterval observationsPeriod;
-    private String beskrivning;
-    private Prognos prognos;
-    private List<PhysicalQuantity> varde;
+	private Id observationsId;
+	private Kod observationsKategori;
+	private Kod observationsKod;
+	private LocalDateTime observationsTid;
+	private PartialInterval observationsPeriod;
+	private String beskrivning;
+	private Prognos prognos;
+	private List<PhysicalQuantity> varde;
+	private Boolean forekomst;
+	private Boolean patientInstammer;
+	private List<Utforarroll> utforsAv;
+	private List<Observationssamband> observationssamband;
 
-    public Kod getObservationsKategori() {
-        return observationsKategori;
-    }
+	public Id getObservationsId() {
+		return observationsId;
+	}
 
-    public void setObservationsKategori(Kod observationsKategori) {
-        this.observationsKategori = observationsKategori;
-    }
+	public void setObservationsId(Id observationsId) {
+		this.observationsId = observationsId;
+	}
 
-    public Kod getObservationsKod() {
-        return observationsKod;
-    }
+	public Kod getObservationsKategori() {
+		return observationsKategori;
+	}
 
-    public void setObservationsKod(Kod observationsKod) {
-        this.observationsKod = observationsKod;
-    }
+	public void setObservationsKategori(Kod observationsKategori) {
+		this.observationsKategori = observationsKategori;
+	}
 
-    public PartialInterval getObservationsPeriod() {
-        return observationsPeriod;
-    }
+	public Kod getObservationsKod() {
+		return observationsKod;
+	}
 
-    public void setObservationsPeriod(PartialInterval observationsPeriod) {
-        this.observationsPeriod = observationsPeriod;
-    }
+	public void setObservationsKod(Kod observationsKod) {
+		this.observationsKod = observationsKod;
+	}
 
-    public String getBeskrivning() {
-        return beskrivning;
-    }
+	public PartialInterval getObservationsPeriod() {
+		return observationsPeriod;
+	}
 
-    public void setBeskrivning(String beskrivning) {
-        this.beskrivning = beskrivning;
-    }
+	public void setObservationsPeriod(PartialInterval observationsPeriod) {
+		this.observationsPeriod = observationsPeriod;
+	}
 
-    public Prognos getPrognos() {
-        return prognos;
-    }
+	public String getBeskrivning() {
+		return beskrivning;
+	}
 
-    public void setPrognos(Prognos prognos) {
-        this.prognos = prognos;
-    }
+	public void setBeskrivning(String beskrivning) {
+		this.beskrivning = beskrivning;
+	}
 
-    public List<PhysicalQuantity> getVarde() {
-        return varde;
-    }
+	public Prognos getPrognos() {
+		return prognos;
+	}
 
-    public void setVarde(List<PhysicalQuantity> varde) {
-        this.varde = varde;
-    }
+	public void setPrognos(Prognos prognos) {
+		this.prognos = prognos;
+	}
+
+	public List<PhysicalQuantity> getVarde() {
+		return varde;
+	}
+
+	public void setVarde(List<PhysicalQuantity> varde) {
+		this.varde = varde;
+	}
+
+	public LocalDateTime getObservationsTid() {
+		return observationsTid;
+	}
+
+	public void setObservationsTid(LocalDateTime observationsTid) {
+		this.observationsTid = observationsTid;
+	}
+
+	public Boolean getForekomst() {
+		return forekomst;
+	}
+
+	public void setForekomst(Boolean forekomst) {
+		this.forekomst = forekomst;
+	}
+
+	public Boolean getPatientInstammer() {
+		return patientInstammer;
+	}
+
+	public void setPatientInstammer(Boolean patientInstammer) {
+		this.patientInstammer = patientInstammer;
+	}
+
+	public List<Utforarroll> getUtforsAv() {
+		if (utforsAv == null) {
+			utforsAv = new ArrayList<Utforarroll>();
+		}
+		return this.utforsAv;
+	}
+
+	public List<Observationssamband> getObservationssamband() {
+		return observationssamband;
+	}
+
+	public void setObservationssamband(List<Observationssamband> observationssamband) {
+		this.observationssamband = observationssamband;
+	}
 }
