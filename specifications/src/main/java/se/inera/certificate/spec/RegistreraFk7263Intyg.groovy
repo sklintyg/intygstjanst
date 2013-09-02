1239fdf6a1c6497262dd1855d6283b26eb5ad359
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-09-02 10:39:02 +0200

    acceptance tests for wiretapping

--- specifications/src/main/java/se/inera/certificate/spec/RegistreraFk7263Intyg.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/RegistreraFk7263Intyg.groovy
@@ -1,23 +1,20 @@
 package se.inera.certificate.spec
-
 import org.springframework.core.io.ClassPathResource
 import se.inera.certificate.spec.util.WsClientFixture
 import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificate.v3.rivtabp20.RegisterMedicalCertificateResponderInterface
-import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificate.v3.rivtabp20.RegisterMedicalCertificateResponderService
 import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificateresponder.v3.RegisterMedicalCertificateResponseType
 import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificateresponder.v3.RegisterMedicalCertificateType
 
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.Unmarshaller
 import javax.xml.transform.stream.StreamSource
-
 /**
  *
  * @author andreaskaltenbach
  */
 public class RegistreraFk7263Intyg extends WsClientFixture {
 
-    private RegisterMedicalCertificateResponderInterface registerMedicalCertificateResponder
+    RegisterMedicalCertificateResponderInterface registerMedicalCertificateResponder
 
     static String serviceUrl = System.getProperty("service.registerCertificateUrl")
 
---
+++ specifications/src/main/java/se/inera/certificate/spec/RegistreraFk7263IntygHosFK.groovy
@@ -0,0 +1,21 @@
+package se.inera.certificate.spec
+
+import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificate.v3.rivtabp20.RegisterMedicalCertificateResponderInterface
+
+/**
+ *
+ * @author andreaskaltenbach
+ */
+public class RegistreraFk7263IntygHosFK extends RegistreraFk7263Intyg {
+
+    public void execute() {
+        String url = serviceUrl ? serviceUrl : baseUrl + "register-certificate-wiretap/v3.0"
+        registerMedicalCertificateResponder = createClient(RegisterMedicalCertificateResponderInterface.class, url)
+
+        super.execute()
+    }
+
+    public String resultat() {
+        resultAsString(response)
+    }
+}
