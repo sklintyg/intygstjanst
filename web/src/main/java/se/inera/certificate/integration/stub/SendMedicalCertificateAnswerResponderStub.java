39078f7c211b2e053a71d37a44e00aaef816f405
Bjorn Beskow <bb@callistaenterprise.se>
2013-09-04 13:19:15 +0200

    Korrigerat tester för Fråga/Svar. Korrigerat integrationstester via NTjP. Korrigerat testdata (cherry picked from commit 7612e634bc19080782ee8850930a4d3b9bb3c397)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

---
+++ web/src/main/java/se/inera/certificate/integration/stub/SendMedicalCertificateAnswerResponderStub.java
@@ -0,0 +1,89 @@
+package se.inera.certificate.integration.stub;
+
+import static se.inera.certificate.integration.util.ResultOfCallUtil.failResult;
+import static se.inera.certificate.integration.util.ResultOfCallUtil.okResult;
+
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+
+import org.apache.cxf.annotations.SchemaValidation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.w3.wsaddressing10.AttributedURIType;
+
+import riv.insuranceprocess.healthreporting.medcertqa._1.Amnetyp;
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateanswer.v1.rivtabp20.SendMedicalCertificateAnswerResponderInterface;
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateanswerresponder.v1.ObjectFactory;
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateanswerresponder.v1.SendMedicalCertificateAnswerResponseType;
+import se.inera.ifv.insuranceprocess.healthreporting.sendmedicalcertificateanswerresponder.v1.SendMedicalCertificateAnswerType;
+
+import com.google.common.base.Throwables;
+
+/**
+ * @author par.wenaker
+ */
+@Transactional
+@SchemaValidation
+public class SendMedicalCertificateAnswerResponderStub implements
+		SendMedicalCertificateAnswerResponderInterface {
+
+	private Logger logger = LoggerFactory
+			.getLogger(SendMedicalCertificateQuestionResponderStub.class);
+
+	private final JAXBContext jaxbContext;
+
+	@Autowired
+	private FkMedicalCertificatesStore fkMedicalCertificatesStore;
+
+	public SendMedicalCertificateAnswerResponderStub() {
+		try {
+			jaxbContext = JAXBContext
+					.newInstance(SendMedicalCertificateAnswerType.class);
+		} catch (JAXBException e) {
+			throw Throwables.propagate(e);
+		}
+	}
+
+	@Override
+	public SendMedicalCertificateAnswerResponseType sendMedicalCertificateAnswer(
+			AttributedURIType logicalAddress,
+			SendMedicalCertificateAnswerType request) {
+
+		SendMedicalCertificateAnswerResponseType response = new SendMedicalCertificateAnswerResponseType();
+
+		try {
+			String id = request.getAnswer().getLakarutlatande()
+					.getLakarutlatandeId();
+
+			marshalCertificate(request);
+			logger.info("STUB Received answer concerning certificate with id: " + id);
+		} catch (JAXBException e) {
+			response.setResult(failResult("Unable to marshal certificate information"));
+			return response;
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw t;
+		}
+		response.setResult(okResult());
+		return response;
+	}
+
+	private String marshalCertificate(SendMedicalCertificateAnswerType request)
+			throws JAXBException {
+
+		StringWriter stringWriter = new StringWriter();
+
+		JAXBElement<SendMedicalCertificateAnswerType> jaxbElement = new ObjectFactory()
+				.createSendMedicalCertificateAnswer(request);
+
+		jaxbContext.createMarshaller().marshal(jaxbElement, stringWriter);
+
+		return stringWriter.toString();
+	}
+
+}
