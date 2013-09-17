fcb69a927100153432c985de2a4c894ea4e2bf0b
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-09-17 17:01:05 +0200

    removal of aktivitetsid, aktivitetsstatus, motivering, syfte, utforsAv (model migration v1.4->v1.5)

--- model/src/main/java/se/inera/certificate/model/Aktivitet.java
+++ model/src/main/java/se/inera/certificate/model/Aktivitet.java
@@ -7,32 +7,12 @@ import java.util.List;
  * @author andreaskaltenbach
  */
 public class Aktivitet {
-    private Id aktivitetsid;
-	private Kod aktivitetskod;
+    private Kod aktivitetskod;
     private String beskrivning;
     private PartialInterval aktivitetstid;
-    private Kod aktivitetsstatus;
-    private String motivering;
-    private String syfte;
-    private List<Utforarroll> utforsAv;
     private List<Utforarroll> beskrivsAv;
     private Vardenhet utforsVid;
-    
-    
-    public List<Utforarroll> getUtforsAv(){
-    	if (utforsAv == null){
-    		utforsAv = new ArrayList<Utforarroll>();
-    	}
-    	return this.utforsAv;
-    }
-    
-    public List<Utforarroll> getBeskrivsAv(){
-    	if (beskrivsAv == null){
-    		beskrivsAv = new ArrayList<Utforarroll>();
-    	}
-    	return this.beskrivsAv;
-    }
-    
+
     public Kod getAktivitetskod() {
         return aktivitetskod;
     }
@@ -49,51 +29,30 @@ public class Aktivitet {
         this.beskrivning = beskrivning;
     }
 
-	public PartialInterval getAktivitetstid() {
-		return aktivitetstid;
-	}
+    public PartialInterval getAktivitetstid() {
+        return aktivitetstid;
+    }
 
-	public void setAktivitetstid(PartialInterval aktivitetstid) {
-		this.aktivitetstid = aktivitetstid;
-	}
+    public void setAktivitetstid(PartialInterval aktivitetstid) {
+        this.aktivitetstid = aktivitetstid;
+    }
 
-	public Id getAktivitetsid() {
-		return aktivitetsid;
-	}
+    public List<Utforarroll> getBeskrivsAv() {
+        if (beskrivsAv == null) {
+            beskrivsAv = new ArrayList<Utforarroll>();
+        }
+        return this.beskrivsAv;
+    }
 
-	public void setAktivitetsid(Id aktivitetsid) {
-		this.aktivitetsid = aktivitetsid;
-	}
+    public void setBeskrivsAv(List<Utforarroll> beskrivsAv) {
+        this.beskrivsAv = beskrivsAv;
+    }
 
-	public Kod getAktivitetsstatus() {
-		return aktivitetsstatus;
-	}
+    public Vardenhet getUtforsVid() {
+        return utforsVid;
+    }
 
-	public void setAktivitetsstatus(Kod aktivitetsstatus) {
-		this.aktivitetsstatus = aktivitetsstatus;
-	}
-
-	public String getMotivering() {
-		return motivering;
-	}
-
-	public void setMotivering(String motivering) {
-		this.motivering = motivering;
-	}
-
-	public String getSyfte() {
-		return syfte;
-	}
-
-	public void setSyfte(String syfte) {
-		this.syfte = syfte;
-	}
-
-	public Vardenhet getUtforsVid() {
-		return utforsVid;
-	}
-
-	public void setUtforsVid(Vardenhet utforsVid) {
-		this.utforsVid = utforsVid;
-	}
+    public void setUtforsVid(Vardenhet utforsVid) {
+        this.utforsVid = utforsVid;
+    }
 }
