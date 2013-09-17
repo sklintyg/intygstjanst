330fa656a531cb7fdb167a67bd70bbc26c67d368
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-09-17 16:44:17 +0200

    changing cardinality for prognoses from 0..1 to 0..* (model migration v1.0->v1.1)

--- model/src/main/java/se/inera/certificate/model/Observation.java
+++ model/src/main/java/se/inera/certificate/model/Observation.java
@@ -2,110 +2,63 @@ package se.inera.certificate.model;
 
 import java.util.List;
 
-import org.joda.time.LocalDateTime;
-
 /**
  * @author andreaskaltenbach
  */
 public class Observation {
 
-	private Id observationsId;
-	private Kod observationsKategori;
-	private Kod observationsKod;
-	private LocalDateTime observationsTid;
-	private PartialInterval observationsPeriod;
-	private String beskrivning;
-	private Prognos prognos;
-	private List<PhysicalQuantity> varde;
-	private Boolean forekomst;
-	private Boolean patientInstammer;
-	private Utforarroll utforsAv;
+    private Kod observationsKategori;
+    private Kod observationsKod;
+    private PartialInterval observationsPeriod;
+    private String beskrivning;
+    private List<Prognos> prognoser;
+    private List<PhysicalQuantity> varde;
 
-	public Id getObservationsId() {
-		return observationsId;
-	}
+    public Kod getObservationsKategori() {
+        return observationsKategori;
+    }
 
-	public void setObservationsId(Id observationsId) {
-		this.observationsId = observationsId;
-	}
+    public void setObservationsKategori(Kod observationsKategori) {
+        this.observationsKategori = observationsKategori;
+    }
 
-	public Kod getObservationsKategori() {
-		return observationsKategori;
-	}
+    public Kod getObservationsKod() {
+        return observationsKod;
+    }
 
-	public void setObservationsKategori(Kod observationsKategori) {
-		this.observationsKategori = observationsKategori;
-	}
+    public void setObservationsKod(Kod observationsKod) {
+        this.observationsKod = observationsKod;
+    }
 
-	public Kod getObservationsKod() {
-		return observationsKod;
-	}
+    public PartialInterval getObservationsPeriod() {
+        return observationsPeriod;
+    }
 
-	public void setObservationsKod(Kod observationsKod) {
-		this.observationsKod = observationsKod;
-	}
+    public void setObservationsPeriod(PartialInterval observationsPeriod) {
+        this.observationsPeriod = observationsPeriod;
+    }
 
-	public PartialInterval getObservationsPeriod() {
-		return observationsPeriod;
-	}
+    public String getBeskrivning() {
+        return beskrivning;
+    }
 
-	public void setObservationsPeriod(PartialInterval observationsPeriod) {
-		this.observationsPeriod = observationsPeriod;
-	}
+    public void setBeskrivning(String beskrivning) {
+        this.beskrivning = beskrivning;
+    }
 
-	public String getBeskrivning() {
-		return beskrivning;
-	}
+    public List<Prognos> getPrognoser() {
+        return prognoser;
+    }
 
-	public void setBeskrivning(String beskrivning) {
-		this.beskrivning = beskrivning;
-	}
+    public void setPrognoser(List<Prognos> prognoser) {
+        this.prognoser = prognoser;
+    }
 
-	public Prognos getPrognos() {
-		return prognos;
-	}
+    public List<PhysicalQuantity> getVarde() {
+        return varde;
+    }
 
-	public void setPrognos(Prognos prognos) {
-		this.prognos = prognos;
-	}
-
-	public List<PhysicalQuantity> getVarde() {
-		return varde;
-	}
-
-	public void setVarde(List<PhysicalQuantity> varde) {
-		this.varde = varde;
-	}
-
-	public LocalDateTime getObservationsTid() {
-		return observationsTid;
-	}
-
-	public void setObservationsTid(LocalDateTime observationsTid) {
-		this.observationsTid = observationsTid;
-	}
-
-	public Boolean getForekomst() {
-		return forekomst;
-	}
-
-	public void setForekomst(Boolean forekomst) {
-		this.forekomst = forekomst;
-	}
-
-	public Boolean getPatientInstammer() {
-		return patientInstammer;
-	}
-
-	public void setPatientInstammer(Boolean patientInstammer) {
-		this.patientInstammer = patientInstammer;
-	}
-
-	public Utforarroll getUtforsAv() {
-		return utforsAv;
-	}
-
-	public void setUtforsAv(Utforarroll utforsAv) {
-		this.utforsAv = utforsAv;
-	}
+    public void setVarde(List<PhysicalQuantity> varde) {
+        this.varde = varde;
+    }
 }
