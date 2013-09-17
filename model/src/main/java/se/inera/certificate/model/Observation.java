2e0ed513604f5e721a79085836bc56120b280928
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-09-17 16:57:51 +0200

    introduction of observationid (model migration v1.1->v1.2)

--- model/src/main/java/se/inera/certificate/model/Observation.java
+++ model/src/main/java/se/inera/certificate/model/Observation.java
@@ -7,6 +7,7 @@ import java.util.List;
  */
 public class Observation {
 
+    private Id observationid;
     private Kod observationsKategori;
     private Kod observationsKod;
     private PartialInterval observationsPeriod;
@@ -60,5 +61,13 @@ public class Observation {
 
     public void setVarde(List<PhysicalQuantity> varde) {
         this.varde = varde;
+    }
+
+    public Id getObservationid() {
+        return observationid;
+    }
+
+    public void setObservationid(Id observationid) {
+        this.observationid = observationid;
     }
 }
