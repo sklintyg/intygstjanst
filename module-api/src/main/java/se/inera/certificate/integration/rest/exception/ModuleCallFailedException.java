7da06bd02288be0ad4637fe8faee557dc7e64c4f
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-10-04 10:19:01 +0200

    ModuleCallFailedException which is used in WebCert

---
+++ module-api/src/main/java/se/inera/certificate/integration/rest/exception/ModuleCallFailedException.java
@@ -0,0 +1,24 @@
+package se.inera.certificate.integration.rest.exception;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * @author andreaskaltenbach
+ */
+public class ModuleCallFailedException extends RuntimeException {
+
+    private String message;
+    private int statusCode;
+    private String reasonPhrase;
+
+    public ModuleCallFailedException(String message, Response response) {
+        this.message = message;
+        statusCode = response.getStatusInfo().getStatusCode();
+        reasonPhrase = response.getStatusInfo().getReasonPhrase();
+    }
+
+    @Override
+    public String getMessage() {
+        return message + " (module returned status code " + statusCode + " and reason " + reasonPhrase;
+    }
+}
