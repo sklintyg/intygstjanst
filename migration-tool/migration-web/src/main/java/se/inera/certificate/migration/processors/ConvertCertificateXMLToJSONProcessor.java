dddcf15afabfda01c7048001392c0e47194d5fdf
erik forsgren <erik.forsgren@r2m.se>
2013-10-11 14:08:37 +0200

    Added minimal http client for converting using a modules RESTapi

--- migration-tool/migration-web/src/main/java/se/inera/certificate/migration/processors/ConvertCertificateXMLToJSONProcessor.java
+++ migration-tool/migration-web/src/main/java/se/inera/certificate/migration/processors/ConvertCertificateXMLToJSONProcessor.java
@@ -1,19 +1,30 @@
 package se.inera.certificate.migration.processors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import se.inera.certificate.migration.model.Certificate;
 import se.inera.certificate.migration.model.OriginalCertificate;
+import se.inera.certificate.migration.rest.FK7263ConverterClient;
 
-public class ConvertCertificateXMLToJSONProcessor implements ItemProcessor<OriginalCertificate, Certificate> {
+public class ConvertCertificateXMLToJSONProcessor implements
+		ItemProcessor<OriginalCertificate, Certificate> {
+	
+	private static Logger LOG = LoggerFactory.getLogger(ConvertCertificateXMLToJSONProcessor.class);
+	
+	@Autowired
+	private FK7263ConverterClient converter;
 
-    public Certificate process(OriginalCertificate orgCert) throws Exception {
-        
-        Certificate cert = new Certificate(orgCert.getCertificateId());
-        
-        //TODO call converter REST service here
-        
-        return cert;
-    }
+	public Certificate process(OriginalCertificate orgCert) throws Exception {
+		LOG.debug("Starting FK7263ConverterClient");
+		
+		Certificate cert = new Certificate(orgCert.getCertificateId());
+		String convertedJson = converter.convertOriginalCertificate(orgCert);
+		
+		cert.setCertificateJson(convertedJson);
+		return cert;
+	}
 
 }
