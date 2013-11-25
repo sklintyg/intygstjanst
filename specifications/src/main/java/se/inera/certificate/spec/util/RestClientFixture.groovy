9b848172d24d7b3a24fca16f40f76c3c17b6e29f
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-11-25 17:47:18 +0100

    RestClient is now SSL-aware and accepts all server certificates
    (cherry picked from commit 292725d7d98d3dd4e6aecd276bd9caa1c6abe220)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

--- specifications/src/main/java/se/inera/certificate/spec/util/RestClientFixture.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/util/RestClientFixture.groovy
@@ -1,4 +1,13 @@
 package se.inera.certificate.spec.util
+
+import groovyx.net.http.RESTClient
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.conn.ssl.SSLSocketFactory
+
+import java.security.KeyStore
+
+import se.inera.certificate.spec.util.ssl.TestSSLSocketFactory;
+
 /**
  *
  * @author andreaskaltenbach
@@ -6,4 +15,21 @@ package se.inera.certificate.spec.util
 class RestClientFixture {
 
     static String baseUrl = System.getProperty("certificate.baseUrl") + "resources/"
+
+    /**
+     * Creates a RestClient which accepts all server certificates
+     * @return
+     */
+    def createRestClient() {
+        def restClient = new RESTClient(baseUrl)
+
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        trustStore.load(null, null);
+        SSLSocketFactory sf = new TestSSLSocketFactory(trustStore);
+        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+        restClient.client.connectionManager.schemeRegistry.register(new Scheme("https", sf, 443))
+
+        restClient
+    }
 }
