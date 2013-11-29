af9b370e94bd63e25264bb230f03d7bccd8fbf68
Bjorn Beskow <bb@callistaenterprise.se>
2013-11-29 08:32:43 +0100

    INTYG-726: Multipel registrering av intyg ger Error-log-poster.

--- web/src/main/java/se/inera/certificate/integration/RegisterMedicalCertificateResponderImpl.java
+++ web/src/main/java/se/inera/certificate/integration/RegisterMedicalCertificateResponderImpl.java
@@ -6,12 +6,15 @@ import intyg.registreraintyg._1.RegistreraIntygResponderInterface;
 
 import javax.xml.ws.Holder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.w3.wsaddressing10.AttributedURIType;
 
 import se.inera.certificate.common.v1.Utlatande;
 import se.inera.certificate.integration.converter.LakarutlatandeTypeToUtlatandeConverter;
+import se.inera.certificate.logging.LogMarkers;
 import se.inera.certificate.service.CertificateService;
 import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificate.v3.rivtabp20.RegisterMedicalCertificateResponderInterface;
 import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificateresponder.v3.RegisterMedicalCertificateResponseType;
@@ -21,6 +24,8 @@ import se.inera.ifv.insuranceprocess.healthreporting.registermedicalcertificater
  * @author andreaskaltenbach
  */
 public class RegisterMedicalCertificateResponderImpl implements RegisterMedicalCertificateResponderInterface {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RegisterMedicalCertificateResponderImpl.class);
 
     @Autowired
     private RegistreraIntygResponderInterface registreraIntygResponder;
@@ -40,6 +45,10 @@ public class RegisterMedicalCertificateResponderImpl implements RegisterMedicalC
             response.setResult(okResult());
         } catch (DataIntegrityViolationException e) {
             response.setResult(infoResult("Certificate already exists"));
+            String certificateId = request.getLakarutlatande().getLakarutlatandeId();
+            String issuedBy =  request.getLakarutlatande().getSkapadAvHosPersonal().getEnhet().getEnhetsId().getExtension();
+            LOG.warn(LogMarkers.VALIDATION, "Validation warning for intyg " + certificateId +
+                    " issued by " + issuedBy +": Certificate already exists - ignored.");
         }
         return response;
     }
