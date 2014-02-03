13bade48d022329127e11cc5f3eea96311cd5716
Bjorn Beskow <bb@callistaenterprise.se>
2014-02-03 06:30:32 +0100

    Corrected include/exclude patterns. Removed unused helper class.

--- specifications/src/main/java/se/inera/certificate/spec/util/WsClientFixture.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/util/WsClientFixture.groovy
@@ -113,53 +113,9 @@ class WsClientFixture {
 		filter.getInclude().add(".*_WITH_DES_.*");
         filter.getInclude().add(".*_WITH_AES_.*");
 		filter.getInclude().add(".*_WITH_NULL_.*");
-		filter.getInclude().add(".*_DH_anon_.*");
+		filter.getExclude().add(".*_DH_anon_.*");
 		tlsParams.setCipherSuitesFilter(filter);//set all the needed include filters.
 
 		httpConduit.setTlsClientParameters(tlsParams);
-	}
-}
-
-/**
- * This class allow any X509 certificates to be used to authenticate the remote side of a secure socket, including
- * self-signed certificates.
- */
-public class TrustAllX509TrustManager
-	implements X509TrustManager
-{
-
-	/** Empty array of certificate authority certificates. */
-	private static final X509Certificate[] acceptedIssuers = [];
-
-	/**
-	 * Always trust for client SSL chain peer certificate chain with any authType authentication types.
-	 *
-	 * @param chain the peer certificate chain.
-	 * @param authType the authentication type based on the client certificate.
-	 */
-	public void checkClientTrusted( X509Certificate[] chain, String authType )
-	{
-		System.err.println "Yes, client is trusted, type is ${authType}"
-	}
-
-	/**
-	 * Always trust for server SSL chain peer certificate chain with any authType exchange algorithm types.
-	 *
-	 * @param chain the peer certificate chain.
-	 * @param authType the key exchange algorithm used.
-	 */
-	public void checkServerTrusted( X509Certificate[] chain, String authType )
-	{
-		System.err.println "Yes, server is trusted, type is ${authType}"
-	}
-
-	/**
-	 * Return an empty array of certificate authority certificates which are trusted for authenticating peers.
-	 *
-	 * @return a empty array of issuer certificates.
-	 */
-	public X509Certificate[] getAcceptedIssuers()
-	{
-		return ( acceptedIssuers );
 	}
 }
