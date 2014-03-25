4a2d0fd339eb2d8871aea81449ad7ac83ee52d9f
Bjorn Beskow <bb@callistaenterprise.se>
2014-03-25 07:16:36 +0100

    Bootstrap-data för dev-profil.
    (cherry picked from commit d7c37444bbd843b322cd1da4db16a46bda1b4a5b)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

--- web/src/main/java/se/inera/certificate/service/util/IntygBootstrapBean.java
+++ web/src/main/java/se/inera/certificate/service/util/IntygBootstrapBean.java
@@ -5,41 +5,65 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import se.inera.certificate.integration.json.CustomObjectMapper;
-import se.inera.certificate.integration.test.CertificateResource;
 import se.inera.certificate.model.dao.Certificate;
 
 public class IntygBootstrapBean {
     private static final Logger LOG = LoggerFactory.getLogger(IntygBootstrapBean.class);
     
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private TransactionTemplate transactionTemplate;
     @Autowired
-    private CertificateResource certificateResource;
+    public void setTxManager(PlatformTransactionManager txManager) {
+        this.transactionTemplate = new TransactionTemplate(txManager); 
+    }
 
     @PostConstruct
     public void initData() {
 
-        List<Resource> files = getResourceListing("bootstrap-intyg/*.json");
-        for (Resource res : files) {
-            LOG.debug("Loading resource " + res.getFilename());
-            addIntyg(res);
+        List<Resource> metadataFiles = getResourceListing("bootstrap-intyg/*-metadata.json");
+        List<Resource> contentFiles = getResourceListing("bootstrap-intyg/*-content.json");
+        int count = metadataFiles.size();
+        for (int i = 0; i < count; i++) {
+            Resource metadata = metadataFiles.get(i);
+            Resource content = contentFiles.get(i);
+            LOG.debug("Loading metadata " + metadata.getFilename() + " and content " + content.getFilename());
+            addIntyg(metadata, content);
         }
     }
 
-    private void addIntyg(Resource res) {
-
-        try {
-            Certificate certificate = new CustomObjectMapper().readValue(res.getInputStream(), BootstrapCertificate.class);
-            certificateResource.insertCertificate(certificate);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private void addIntyg(final Resource metadata, final Resource content) {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                    Certificate certificate = new CustomObjectMapper().readValue(metadata.getInputStream(), Certificate.class);
+                    certificate.setDocument(IOUtils.toString(content.getInputStream(), "UTF-8"));            
+                    entityManager.persist(certificate);
+                } catch (Throwable t) {
+                    status.setRollbackOnly();
+                    LOG.warn("Loading failed: " + t.getMessage());
+                }
+            }
+        });
 
     }
 
