a357565929944955bd65bc6e31d84e9a551f5ae0
Bjorn Beskow <bb@callistaenterprise.se>
2014-04-30 10:29:58 +0200

    Lagt till stöd för https utan klient-certifikat.

--- specifications/src/main/java/se/inera/certificate/spec/util/WsClientFixture.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/util/WsClientFixture.groovy
@@ -61,7 +61,7 @@ class WsClientFixture {
 		factory.setAddress(url);
 		def responder = factory.create();
 		if (url.startsWith("https:")) {
-			setClientCertificate(responder)
+			setupSSLCertificates(responder)
 		}
 		return responder
 	}
@@ -80,33 +80,37 @@ class WsClientFixture {
 		else null
 	}
 
-	def setClientCertificate(def responder) {
+	def setupSSLCertificates(def responder) {
+        boolean ntjpClientAuthentication = Boolean.getBoolean("service.ntjpClientAuthentication")
 		Client client = ClientProxy.getClient(responder)
 		HTTPConduit httpConduit = (HTTPConduit)client.getConduit();
 		TLSClientParameters tlsParams = new TLSClientParameters();
 		tlsParams.setDisableCNCheck(true);
 
-		KeyStore trustStore = KeyStore.getInstance("JKS");
-		String trustpass = "password";//provide trust pass
-
-		trustStore.load(WsClientFixture.class.getResourceAsStream("/truststore-ntjp.jks"), trustpass.toCharArray());
-		TrustManagerFactory trustFactory =
-				TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		trustFactory.init(trustStore);
-		TrustManager[] tm = trustFactory.getTrustManagers();
-		// TrustManager[] tm = [new TrustAllX509TrustManager()]
-		tlsParams.setTrustManagers(tm);
-
-		KeyStore certStore = KeyStore.getInstance("PKCS12");
-        String certFile = System.getProperty("ws.certificate.file");
-        String certPass = System.getProperty("ws.certificate.password");
-		certStore.load(WsClientFixture.class.getResourceAsStream(certFile), certPass.toCharArray());
-		KeyManagerFactory keyFactory =
-				KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-		keyFactory.init(certStore, certPass.toCharArray());
-		KeyManager[] km = keyFactory.getKeyManagers();
-		tlsParams.setKeyManagers(km);
-
+        if (ntjpClientAuthentication) {
+    		KeyStore trustStore = KeyStore.getInstance("JKS");
+    		String trustpass = "password";//provide trust pass
+    
+    		trustStore.load(WsClientFixture.class.getResourceAsStream("/truststore-ntjp.jks"), trustpass.toCharArray());
+    		TrustManagerFactory trustFactory =
+    				TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+    		trustFactory.init(trustStore);
+    		TrustManager[] tm = trustFactory.getTrustManagers();
+    		tlsParams.setTrustManagers(tm);
+    
+    		KeyStore certStore = KeyStore.getInstance("PKCS12");
+            String certFile = System.getProperty("ws.certificate.file");
+            String certPass = System.getProperty("ws.certificate.password");
+    		certStore.load(WsClientFixture.class.getResourceAsStream(certFile), certPass.toCharArray());
+    		KeyManagerFactory keyFactory =
+    				KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+    		keyFactory.init(certStore, certPass.toCharArray());
+    		KeyManager[] km = keyFactory.getKeyManagers();
+    		tlsParams.setKeyManagers(km);
+        } else {
+        TrustManager[] tm = [new TrustAllX509TrustManager()]
+        tlsParams.setTrustManagers(tm);
+    }
 		FiltersType filter = new FiltersType();
 		filter.getInclude().add(".*_EXPORT_.*");
 		filter.getInclude().add(".*_EXPORT1024_.*");
@@ -118,4 +122,42 @@ class WsClientFixture {
 
 		httpConduit.setTlsClientParameters(tlsParams);
 	}
+
+    /**
+     * This class allow any X509 certificates to be used to authenticate the remote side of a secure socket, including
+     * self-signed certificates.
+     */
+    public class TrustAllX509TrustManager implements X509TrustManager {
+
+        /** Empty array of certificate authority certificates. */
+        private static final X509Certificate[] acceptedIssuers = [];
+
+        /**
+         * Always trust for client SSL chain peer certificate chain with any authType authentication types.
+         *
+         * @param chain the peer certificate chain.
+         * @param authType the authentication type based on the client certificate.
+         */
+        public void checkClientTrusted( X509Certificate[] chain, String authType ) {
+        }
+
+        /**
+         * Always trust for server SSL chain peer certificate chain with any authType exchange algorithm types.
+         *
+         * @param chain the peer certificate chain.
+         * @param authType the key exchange algorithm used.
+         */
+        public void checkServerTrusted( X509Certificate[] chain, String authType ) {
+        }
+
+        /**
+         * Return an empty array of certificate authority certificates which are trusted for authenticating peers.
+         *
+         * @return a empty array of issuer certificates.
+         */
+        public X509Certificate[] getAcceptedIssuers() {
+            return ( acceptedIssuers );
+        }
+    }
+
 }
