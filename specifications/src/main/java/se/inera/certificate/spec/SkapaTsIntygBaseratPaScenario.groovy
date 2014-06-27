69086950aa2c26ab31a2c0f6c9b757c98a181552
Erik Forsgren <erik.forsgren@r2m.se>
2014-06-27 08:49:30 +0200

    Ändrade fitnesse-fixtur för ts-testdata så att personnummer kan anges

--- specifications/src/main/java/se/inera/certificate/spec/SkapaTsIntygBaseratPaScenario.groovy
+++ specifications/src/main/java/se/inera/certificate/spec/SkapaTsIntygBaseratPaScenario.groovy
@@ -1,20 +1,18 @@
 package se.inera.certificate.spec
 
+import java.util.UUID.*
+
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.Unmarshaller
 import javax.xml.transform.stream.StreamSource
 
-import java.util.UUID.*
-
-import org.joda.time.LocalDateTime
 import org.springframework.core.io.ClassPathResource
 
-import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateResponderInterface;
-import se.inera.certificate.spec.util.WsClientFixture
-import se.inera.certificate.spec.util.WsClientFixtureNyaKontraktet;
-import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateResponseType;
-import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateResponderService;
+import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateResponderInterface
+import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateResponseType
 import se.inera.certificate.clinicalprocess.healthcond.certificate.registerCertificate.v1.RegisterCertificateType
+import se.inera.certificate.spec.util.WsClientFixture
+import se.inera.certificate.spec.util.WsClientFixtureNyaKontraktet
 
 public class SkapaTsIntygBaseratPaScenario extends WsClientFixtureNyaKontraktet {
 
@@ -25,10 +23,9 @@ public class SkapaTsIntygBaseratPaScenario extends WsClientFixtureNyaKontraktet 
 
 	String mall
     String typ
+    String personnummer
 	String antalIntyg
-	File personFile
 	
-	def personnummer = []
 	def responses = []
 	
 	// Used to iterate through list of personnummer 
@@ -45,35 +42,14 @@ public class SkapaTsIntygBaseratPaScenario extends WsClientFixtureNyaKontraktet 
 		super(logiskAddress)
 		String url = serviceUrl ? serviceUrl : baseUrl + "register-certificate/v1.0"
         registerCertificateResponder = createClient(RegisterCertificateResponderInterface.class, url)
-		personFile = new ClassPathResource(System.getProperty("persondata")).getFile()
-		personFile.eachLine { line -> personnummer << line }
 	}
 	
-	/**
-	 * Go through the list of personnummer, 
-	 * when the end is reached start over from the beginning.
-	 * 
-	 * @return a personnummer as a String
-	 */
-	private String getNextPersonnummer() {
-		String ret
-		if (personnummer[index] != null) {
-			ret = personnummer[index]
-			index++
-		}
-		else {
-			index = 0
-			ret = personnummer[index]
-		}
-		return ret
-	}
-
 	public void execute() {
 		
 		for (int i = 0; i < Integer.parseInt(antalIntyg); i++) {
 			RegisterCertificateType request = unmarshaller.unmarshal(new StreamSource(new ClassPathResource("grundladda/" + typ + "/" + mall).getInputStream()), RegisterCertificateType.class).getValue()
 
-			request.utlatande.patient.personId.extension = getNextPersonnummer()
+			request.utlatande.patient.personId.extension = personnummer
 			request.utlatande.utlatandeId.extension = uid.randomUUID()
 			response = registerCertificateResponder.registerCertificate(logicalAddress.toString(), request)
 			
