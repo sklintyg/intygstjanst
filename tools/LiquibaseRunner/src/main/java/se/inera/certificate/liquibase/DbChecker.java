547f14126002e4d06b4a1f4f79b9245ed4d6cbd5
Pierre Sandboge <pierre.sandboge@standin.se>
2014-08-07 14:31:08 +0200

    INTYG-1212 Simplified solution by using logicalFilePath in changelog.

--- tools/LiquibaseRunner/src/main/java/se/inera/certificate/liquibase/DbChecker.java
+++ tools/LiquibaseRunner/src/main/java/se/inera/certificate/liquibase/DbChecker.java
@@ -1,4 +1,4 @@
-package se.inera.certificate.persistence.liquibase;
+package se.inera.certificate.liquibase;
 
 import liquibase.Liquibase;
 import liquibase.changelog.ChangeSet;
@@ -11,8 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -23,7 +21,7 @@ public class DbChecker {
         try {
             DatabaseConnection connection = new JdbcConnection(dataSource.getConnection());
             Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(connection);
-            Liquibase liquibase = new Liquibase(script, new ClassPathResourceAccessor(), database);
+            Liquibase liquibase = new Liquibase(script, new ClassLoaderResourceAccessor(), database);
             LOG.info(database.getConnection().getURL());
             List<ChangeSet> changeSets = liquibase.listUnrunChangeSets(null);
             if (!changeSets.isEmpty()) {
@@ -33,27 +31,11 @@ public class DbChecker {
                 }
                 throw new Error("Database version mismatch. Check liquibase status. Errors:\n" + errors.toString() + database.getDatabaseProductName() + ", " + database);
             }
-        } catch (liquibase.exception.LiquibaseException | SQLException e) {
+        } catch (liquibase.exception.LiquibaseException e) {
+            throw new Error("Database not ok, aborting startup.", e);
+        } catch (SQLException e) {
             throw new Error("Database not ok, aborting startup.", e);
         }
         LOG.info("Liquibase ok");
-    }
-
-    /**
-     * We have used SpringLiquibase, which has the undesirable trait that it prepends "classpath:" to filenames stored
-     * in liquibase.
-     *
-     * This class is a workaround to make non-Springliquibase code work with SpringLiquibase-managed databases.
-     * An alternative solution would be to scrap SpringLiquibase altogether, but then the FILENAME needs to be
-     * updated in the database. (E.g. update databasechangelog set filename='changelog/changelog.xml')
-     */
-    private class ClassPathResourceAccessor extends ClassLoaderResourceAccessor {
-        @Override
-        public InputStream getResourceAsStream(String file) throws IOException {
-            if (file.startsWith("classpath:")) {
-                file = file.substring(file.indexOf(':') + 1);
-            }
-            return super.getResourceAsStream(file);
-        }
     }
 }
--- tools/LiquibaseRunner/src/main/java/se/inera/certificate/liquibase/Runner.java
+++ tools/LiquibaseRunner/src/main/java/se/inera/certificate/liquibase/Runner.java
@@ -9,7 +9,6 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,13 +17,13 @@ import java.util.Properties;
 
 public class Runner {
     public static void main(String[] args) {
-        String script = "classpath:changelog/changelog.xml";
+        String script = "changelog/changelog.xml";
         System.out.println(System.getProperty("java.class.path"));
 
         try {
             DatabaseConnection connection = new JdbcConnection(getConnection());
             Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(connection);
-            Liquibase liquibase = new Liquibase(script, new ClassPathResourceAccessor(), database);
+            Liquibase liquibase = new Liquibase(script, new ClassLoaderResourceAccessor(), database);
             System.out.println(database.getConnection().getURL());
             liquibase.update(null);
             List<ChangeSet> changeSets = liquibase.listUnrunChangeSets(null);
@@ -54,25 +53,5 @@ public class Runner {
         Properties properties = new Properties();
         properties.load(Runner.class.getClassLoader().getResourceAsStream("runner.properties"));
         return properties;
-    }
-
-    /**
-     * We have used SpringLiquibase, which has the undesirable trait that it prepends "classpath:" to filenames stored
-     * in liquibase.
-     *
-     * This class is a workaround to make non-Springliquibase code work with SpringLiquibase-managed databases.
-     * An alternative solution would be to scrap SpringLiquibase altogether, but then the FILENAME needs to be
-     * updated in the database. (E.g. update databasechangelog set filename='changelog/changelog.xml')
-     */
-    private static class ClassPathResourceAccessor extends ClassLoaderResourceAccessor {
-        @Override
-        public InputStream getResourceAsStream(String file) throws IOException {
-            System.out.println(file);
-            if (file.startsWith("classpath:")) {
-                file = file.substring(file.indexOf(':') + 1);
-            }
-            System.out.println(file);
-            return super.getResourceAsStream(file);
-        }
     }
 }
