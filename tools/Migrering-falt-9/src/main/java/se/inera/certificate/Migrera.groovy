d7eae0fd888637f25131ee84f6572270655ea7af
Bjorn Beskow <bb@callistaenterprise.se>
2014-09-10 15:02:35 +0200

    Lagt till XML parsning/generering för SendMedicalCertificate

--- tools/Migrering-falt-9/src/main/java/se/inera/certificate/Migrera.groovy
+++ tools/Migrering-falt-9/src/main/java/se/inera/certificate/Migrera.groovy
@@ -33,7 +33,7 @@ class Migrera {
                                 username: config.dataSource.username, password: config.dataSource.password,
                                 initialSize: numberOfThreads, maxTotal: numberOfThreads)
         def bootstrapSql = new Sql(dataSource)
-        def allaIntyg = bootstrapSql.rows("""
+        def certificateIds = bootstrapSql.rows("""
             select oc.CERTIFICATE_ID from ORIGINAL_CERTIFICATE oc
             where oc.DOCUMENT like '%motivering>%' 
             and oc.DOCUMENT not like '%prognosangivelse>%'
@@ -44,11 +44,69 @@ class Migrera {
             )""")
         bootstrapSql.close()
         
+        def output = migrateCertificates(certificateIds, dataSource, numberOfThreads) 
+        long end = System.currentTimeMillis()
+        output.each {line ->
+            if (line) println line
+        }
+        println "${output.size()} certificates migrated in ${end-start} milliseconds"
+        
+        Sql sql = new Sql(dataSource)
+        int sent = 0
+        int error = 0
+        int info = 0
+        certificateIds.each {
+            def id = it.CERTIFICATE_ID
+            def sentState = sql.rows("""
+                select cs1.CERTIFICATE_ID from CERTIFICATE_STATE cs1
+                left join CERTIFICATE_STATE cs2 on cs1.CERTIFICATE_ID = cs2.CERTIFICATE_ID
+                where cs1.CERTIFICATE_ID = :id
+                and cs1.STATE = 'RECEIVED' 
+                and cs2.STATE = 'SENT'
+                and TIMESTAMPDIFF(SECOND, cs1.TIMESTAMP, cs2.TIMESTAMP) > 1
+                and cs1.CERTIFICATE_ID not in (
+                    select cs3.CERTIFICATE_ID from CERTIFICATE_STATE cs3
+                    where cs3.STATE = 'CANCELLED')
+                """, [id : id])
+            if (sentState) {
+                def row = sql.firstRow('select DOCUMENT from ORIGINAL_CERTIFICATE where CERTIFICATE_ID = :id' , [id : id])
+                def sendMessage = buildSendMessage(new String(row.DOCUMENT, 'UTF-8'), id)
+                def http = new HTTPBuilder( config.webService.send.URL , XML )
+                http.request( POST, XML ) {
+                    headers."Content-Type" = "application/xml; charset=utf-8"
+                    headers."SOAPAction" = config.webService.send.SOAPAction
+                    body = sendMessage.toString().replace('\\n', '\n')
+    
+                    response.success = { resp, xmlResponse ->
+                        if (xmlResponse.Body.SendMedicalCertificateResponse.result.resultCode == 'INFO') {
+                            println "${id} [INFO - ${xmlResponse.Body.SendMedicalCertificateResponse.result.infoText}]"
+                            info++
+                            sent++
+                        } else if (xmlResponse.Body.SendMedicalCertificateResponse.result.resultCode == 'ERROR') {
+                            println "${id} [ERROR - ${xmlResponse.Body.SendMedicalCertificateResponse.result.errorText}]"
+                            error++
+                        } else {
+                            sent++
+                        }
+                    }
+                    response.failure = { resp, xmlResponse ->
+                        println "failure: ${xmlResponse}"
+                        error++
+                    }
+                }
+                Thread.sleep(1)
+            }
+        }
+        sql.close()
+        println "$sent certificates sent to FK, $info with info messages, $error errors"
+    }
+
+    static def migrateCertificates(def certificateIds, def dataSource, int numberOfThreads) {
         final AtomicInteger count = new AtomicInteger(0)
-        def output
+        def results
         GParsPool.withPool(numberOfThreads) {
-            output = allaIntyg.collectParallel {
-                StringBuffer result = new StringBuffer() 
+            results = certificateIds.collectParallel {
+                StringBuffer result = new StringBuffer()
                 def id = it.CERTIFICATE_ID
                 Sql sql = new Sql(dataSource)
                 def row = sql.firstRow( 'select DOCUMENT from ORIGINAL_CERTIFICATE where CERTIFICATE_ID = :id' , [id : id])
@@ -63,34 +121,61 @@ class Migrera {
                 result.toString()
             }
         }
-        output.each {line ->
-            if (line) println line
-        }
-        long end = System.currentTimeMillis()
-        println "$count certificates migrated in ${end-start} milliseconds"
-
-        Sql sql = new Sql(dataSource)
-        int sent = 0
-        allaIntyg.each {
-            def id = it.CERTIFICATE_ID
-            def sentState = sql.rows( "select cs1.CERTIFICATE_ID from CERTIFICATE_STATE cs1 " +
-                                      "left join CERTIFICATE_STATE cs2 on cs1.CERTIFICATE_ID = cs2.CERTIFICATE_ID " +
-                                      "where cs1.CERTIFICATE_ID = :id " +
-                                      "and cs1.STATE = 'RECEIVED' " +
-                                      "and cs2.STATE = 'SENT' "+
-                                      "and TIMESTAMPDIFF(SECOND, cs1.TIMESTAMP, cs2.TIMESTAMP) > 1 " +
-                                      "and cs1.CERTIFICATE_ID not in (" +
-                                      "  select cs3.CERTIFICATE_ID from CERTIFICATE_STATE cs3 " +
-                                      "  where cs3.STATE = 'CANCELLED')", [id : id])
-            if (sentState) {
-                def row = sql.firstRow( 'select DOCUMENT from ORIGINAL_CERTIFICATE where CERTIFICATE_ID = :id' , [id : id])
-                println "$id should be sent"
-                sent++
-                Thread.sleep(1)
+        results
+    }
+    
+    static def buildSendMessage(def registerXml, def id) {
+        def xml = new XmlSlurper().parseText(registerXml)
+        xml.declareNamespace(ns1: 'urn:riv:insuranceprocess:healthreporting:mu7263:3',
+            qa: 'urn:riv:insuranceprocess:healthreporting:medcertqa:1',
+            ns2: 'urn:riv:insuranceprocess:healthreporting:2',
+            ns3: 'urn:riv:insuranceprocess:healthreporting:RegisterMedicalCertificateResponder:3',
+            s: 'http://schemas.xmlsoap.org/soap/envelope/')
+        def intyg = xml.'ns3:lakarutlatande'
+        def avsant = intyg.'ns1:skickatDatum'.text()
+        def signering = intyg.'ns1:signeringsdatum'.text()
+        def patientId = intyg.'ns1:patient'.'ns2:person-id'
+        def patientNamn = intyg.'ns1:patient'.'ns2:fullstandigtNamn'.text()
+        def hosPersonalId = intyg.'ns1:skapadAvHosPersonal'.'ns2:personal-id'
+        def hosPersonalNamn = intyg.'ns1:skapadAvHosPersonal'.'ns2:fullstandigtNamn'.text()
+        def enhet = intyg.'ns1:skapadAvHosPersonal'.'ns2:enhet'
+        def builder = new StreamingMarkupBuilder()
+        builder.encoding = "UTF-8"
+        def doc = builder.bind {
+            mkp.xmlDeclaration()
+            mkp.declareNamespace(s: 'http://schemas.xmlsoap.org/soap/envelope/')
+            's:Envelope' {
+                's:Header' {
+                    mkp.declareNamespace(h: 'http://www.w3.org/2005/08/addressing')
+                    'h:To'("2021005521")
+                }
+                's:Body' {
+                    mkp.declareNamespace(core: 'urn:riv:insuranceprocess:healthreporting:2',
+                    qa: 'urn:riv:insuranceprocess:healthreporting:medcertqa:1')
+                    SendMedicalCertificateRequest(xmlns: 'urn:riv:insuranceprocess:healthreporting:SendMedicalCertificateResponder:1') {
+                        send {
+                            'vardReferens-id'("Migrera-fält9-$id")
+                            'avsantTidpunkt'(avsant)
+                            'adressVard' {
+                                'qa:hosPersonal' {
+                                    mkp.yield hosPersonalId
+                                    'core:fullstandigtNamn'(hosPersonalNamn)
+                                    mkp.yield enhet
+                                }
+                            }
+                            'lakarutlatande' {
+                                'qa:lakarutlatande-id'(id)
+                                'qa:signeringsTidpunkt'(signering)
+                                'qa:patient' {
+                                     mkp.yield patientId
+                                     'core:fullstandigtNamn'(patientNamn)
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        sql.close()
-        println "$sent certificates sent to FK"
+        doc.toString()
     }
-
 }
