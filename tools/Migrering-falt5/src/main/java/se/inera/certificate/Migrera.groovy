f319899c1993cc8b0cb710d25ac3afd91c6c2a69
Erik Forsgren <erik.forsgren@r2m.se>
2014-10-17 12:02:26 +0200

    Migreringstool för WEBCERT-1106

---
+++ tools/Migrering-falt5/src/main/java/se/inera/certificate/Migrera.groovy
@@ -0,0 +1,78 @@
+package se.inera.certificate
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.sql.Sql
+import groovyx.gpars.GParsPool
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.apache.commons.dbcp2.BasicDataSource
+
+class Migrera {
+
+    static void main(String[] args) {
+        int numberOfThreads = args.length > 0 ? args[0] : 10
+        long start = System.currentTimeMillis()
+        def props = new Properties()
+        new File("dataSource.properties").withInputStream { stream ->
+            props.load(stream)
+        }
+        def config = new ConfigSlurper().parse(props)
+        BasicDataSource dataSource =
+            new BasicDataSource(driverClassName: config.dataSource.driver, url: config.dataSource.url,
+                                username: config.dataSource.username, password: config.dataSource.password,
+                                initialSize: numberOfThreads, maxTotal: numberOfThreads)
+        def bootstrapSql = new Sql(dataSource)
+        def allaIntyg = bootstrapSql.rows("select ID from CERTIFICATE")
+        bootstrapSql.close()
+        final AtomicInteger count = new AtomicInteger(0)
+        final AtomicInteger countUpdated = new AtomicInteger(0)
+        def output
+        GParsPool.withPool(numberOfThreads) {
+            output = allaIntyg.collectParallel {
+                StringBuffer result = new StringBuffer() 
+                def id = it.ID
+                Sql sql = new Sql(dataSource)
+                def row = sql.firstRow( 'select DOCUMENT from CERTIFICATE where ID = :id' , [id : id])
+                def document = new JsonSlurper().parseText(new String(row.DOCUMENT, 'UTF-8'))
+                // Check whether observations with the observation code 'd' contain a 'beskrivning' 
+                // if not, delete the observation since it is incorrect.
+                def observationer = document.observationer
+                boolean jsonUpdated = false
+                if (observationer != null) {
+                    for (int i = 0; i < observationer.size; i++) {
+                        if (observationer[i].observationskategori != null) {
+                            if(observationer[i].observationskategori.code == 'd' && !observationer[i].beskrivning) {
+                                observationer.remove(observationer[i])
+                                jsonUpdated = true
+                            }
+                        }
+                    }
+                }
+                // If json is updated, convert back to byte array
+                def updatedDocument = jsonUpdated ? new JsonBuilder(document).toString().getBytes('UTF-8') : null
+
+                if (updatedDocument) {
+                    sql.execute('update CERTIFICATE set DOCUMENT = :document where ID = :id' , [document: updatedDocument, id : id])
+                    result << "$id updated - removed faulty observation"
+                    countUpdated.addAndGet(1)
+                } 
+                sql.close()
+                int current = count.addAndGet(1)
+                if (current % 10000 == 0) {
+                    println current
+                }
+                result.toString()
+            }
+        }
+        long end = System.currentTimeMillis()
+        output.each {line ->
+            if (line) println line
+        }
+        println "$count certificates checked in ${end-start} milliseconds"
+        println "$countUpdated certificates updated"
+    }
+
+}
