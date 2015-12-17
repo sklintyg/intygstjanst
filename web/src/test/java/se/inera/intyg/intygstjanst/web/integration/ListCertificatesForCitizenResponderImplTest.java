5e78fb6c319bdfeb01b626dbf930b4975124bf46
Fredrik Engstrom <fredrik.engstrom@callistaenterprise.se>
2015-12-17 15:48:59 +0100

    WEBCERT-1198: Har lagt till licens-header

--- web/src/test/java/se/inera/intyg/intygstjanst/web/integration/ListCertificatesForCitizenResponderImplTest.java
+++ web/src/test/java/se/inera/intyg/intygstjanst/web/integration/ListCertificatesForCitizenResponderImplTest.java
@@ -1,16 +1,16 @@
-/**
- * Copyright (C) 2013 Inera AB (http://www.inera.se)
+/*
+ * Copyright (C) 2015 Inera AB (http://www.inera.se)
  *
- * This file is part of Inera Certificate (http://code.google.com/p/inera-certificate).
+ * This file is part of sklintyg (https://github.com/sklintyg).
  *
- * Inera Certificate is free software: you can redistribute it and/or modify
+ * sklintyg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Inera Certificate is distributed in the hope that it will be useful,
+ * sklintyg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
