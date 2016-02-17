7e54c928cdebb8a5a38b393bd8916e1036161b5a
Katarina Olsson <katarina.olsson@r2m.se>
2016-02-17 14:09:17 +0100

    SIT-12: Minor cleanup and completion of test case.

--- persistence/src/main/java/se/inera/intyg/intygstjanst/persistence/model/dao/SendMessageToCareRepository.java
+++ persistence/src/main/java/se/inera/intyg/intygstjanst/persistence/model/dao/SendMessageToCareRepository.java
@@ -2,7 +2,6 @@ package se.inera.intyg.intygstjanst.persistence.model.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface SendMessageToCareRepository extends JpaRepository<SendMessageToCare, Long>{
 
 }
