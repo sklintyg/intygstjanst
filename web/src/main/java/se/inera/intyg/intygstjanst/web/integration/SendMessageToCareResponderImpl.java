d3b2a9dd1e6b54e895638dfaa507fe4b7e058e8e
Katarina Olsson <katarina.olsson@r2m.se>
2016-02-17 11:44:58 +0100

    SIT-12: Up and running the webservice.

--- web/src/main/java/se/inera/intyg/intygstjanst/web/integration/SendMessageToCareResponderImpl.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/SendMessageToCareResponderImpl.java
@@ -1,6 +1,5 @@
 package se.inera.intyg.intygstjanst.web.integration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 import se.riv.clinicalprocess.healthcond.certificate.sendMessageToCare.v1.SendMessageToCareResponderInterface;
 import se.riv.clinicalprocess.healthcond.certificate.sendMessageToCare.v1.SendMessageToCareResponseType;
@@ -9,22 +8,15 @@ import se.riv.clinicalprocess.healthcond.certificate.v2.ResultCodeType;
 import se.riv.clinicalprocess.healthcond.certificate.v2.ResultType;
 
 public class SendMessageToCareResponderImpl implements SendMessageToCareResponderInterface{
-
-    
-    @Autowired
-    private SendMessageToCareResponderInterface forwarder;
     
     @Override
     public SendMessageToCareResponseType sendMessageToCare(String logicalAddress, SendMessageToCareType parameters) {
         SendMessageToCareResponseType response = new SendMessageToCareResponseType();
         
-        
-       // forwarder.sendMessageToCare(parameters.getLogiskAdressMottagare().getExtension(), parameters);
-        
         ResultType resultType = new ResultType();
         resultType.setResultCode(ResultCodeType.OK);
         response.setResult(resultType);
-        return null;
+        return response;
     }
 
 }
