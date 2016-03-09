7b890eb925b845a4b8635565834bb3d89a47f1d4
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2016-03-09 13:42:06 +0100

    INTYG-2040: Första tom implementation av ListCertificatesForCitizen

---
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/ListCertificatesForCitizenResponderImpl2.java
@@ -0,0 +1,14 @@
+package se.inera.intyg.intygstjanst.web.integration;
+
+import se.riv.clinicalprocess.healthcond.certificate.listCertificatesForCitizen.v2.ListCertificatesForCitizenResponderInterface;
+import se.riv.clinicalprocess.healthcond.certificate.listCertificatesForCitizen.v2.ListCertificatesForCitizenResponseType;
+import se.riv.clinicalprocess.healthcond.certificate.listCertificatesForCitizen.v2.ListCertificatesForCitizenType;
+
+public class ListCertificatesForCitizenResponderImpl2 implements ListCertificatesForCitizenResponderInterface {
+
+    @Override
+    public ListCertificatesForCitizenResponseType listCertificatesForCitizen(String s, ListCertificatesForCitizenType listCertificatesForCitizenType) {
+        return null;
+    }
+
+}
