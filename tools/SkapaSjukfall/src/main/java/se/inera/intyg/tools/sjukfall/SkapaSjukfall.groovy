e11f970fe945c428e0559c302af9713d1c00c4b9
Erik Lupander <erik.lupander@callistaenterprise.se>
2016-04-08 15:34:05 +0200

    REHAB-224: Nu hanteras ev. existerande data OK av SkapaSjukfall-skriptet

--- tools/SkapaSjukfall/src/main/java/se/inera/intyg/tools/sjukfall/SkapaSjukfall.groovy
+++ tools/SkapaSjukfall/src/main/java/se/inera/intyg/tools/sjukfall/SkapaSjukfall.groovy
@@ -53,8 +53,11 @@ class SkapaSjukfall {
                                 username: config.dataSource.username, password: config.dataSource.password,
                                 initialSize: numberOfThreads, maxTotal: numberOfThreads)
         def bootstrapSql = new Sql(dataSource)
-        def certificateIds = bootstrapSql.rows("select c.ID from CERTIFICATE c WHERE c.CERTIFICATE_TYPE = :certType AND DELETED = :deleted",
-                                               [certType : 'fk7263', deleted : false])
+
+        // TODO include deleted, we may need to update state of SJUKFALL_CERT
+
+        def certificateIds = bootstrapSql.rows("select c.ID from CERTIFICATE c WHERE c.CERTIFICATE_TYPE = :certType",      //  AND DELETED = :deleted
+                                               [certType : 'fk7263'])                                                      // , deleted : false
         bootstrapSql.close()
                 
         println "- ${certificateIds.size()} candidates for being processed into sjukfall found"
@@ -73,7 +76,7 @@ class SkapaSjukfall {
                 def id = it.ID
                 Sql sql = new Sql(dataSource)
                 try {
-                    def row = sql.firstRow( 'select DOCUMENT from CERTIFICATE where ID = :id'
+                    def row = sql.firstRow( 'select DOCUMENT, DELETED from CERTIFICATE where ID = :id'
                             , [id : id])
                     if (row == null || row.DOCUMENT == null) {
                         println "Intyg ${id} has no DOCUMENT, skipping."
@@ -97,46 +100,55 @@ class SkapaSjukfall {
                     String doctorId = utlatande.grundData.skapadAv.personId
                     String doctorName = utlatande.grundData.skapadAv.fullstandigtNamn
 
-                    Boolean deleted = false
+                    Boolean deleted = row.DELETED;
                     LocalDateTime signingDateTime = utlatande.grundData.signeringsdatum
                     java.sql.Date sqlSigningDateTime = new java.sql.Date(signingDateTime.toDate().getTime())
 
+                    def sjukfallRow = sql.firstRow( 'SELECT id FROM SJUKFALL_CERT sc WHERE sc.id = :id', [id : id])
 
-                    // Insert base sjukfall cert data
-                    sql.execute("INSERT INTO SJUKFALL_CERT "
-                            + "(ID,CERTIFICATE_TYPE,CIVIC_REGISTRATION_NUMBER,PATIENT_FIRST_NAME,PATIENT_LAST_NAME,CARE_UNIT_ID,CARE_UNIT_NAME,CARE_GIVER_ID,SIGNING_DOCTOR_ID,SIGNING_DOCTOR_NAME,DIAGNOSE_CODE,DELETED,SIGNING_DATETIME)"
-                            + "VALUES (:id,:type,:personnummer,:firstName,:lastName,:careUnitId,:careUnitName,:careGiverId,:doctorId,:doctorName,:diagnosKod,:deleted,:signingDateTime)"
-                            , [id: id, type : utlatande.typ, personnummer: personnummer, firstName : firstName, lastName : lastName ,
-                               careUnitId : careUnitId, careUnitName : careUnitName, careGiverId : careGiverId, doctorId : doctorId,
-                               doctorName : doctorName, diagnosKod : diagnosKod, deleted : deleted, signingDateTime : sqlSigningDateTime
-                            ])
+                    // If not exists, just insert.
+                    if (sjukfallRow == null || sjukfallRow.ID == null) {
 
-                    // Insert one item per nedsattning
-                    String insertSql = "INSERT INTO SJUKFALL_CERT_WORK_CAPACITY (CERTIFICATE_ID,CAPACITY_PERCENTAGE,FROM_DATE,TO_DATE) VALUES(:id,:nedsattningProcent,:fromDate,:toDate)";
-                    if (utlatande.nedsattMed25 != null) {
-                        sql.execute(insertSql, [id:id,
-                                                nedsattningProcent: 25,
-                                                fromDate: utlatande.nedsattMed25.fromAsLocalDate().toString("yyyy-MM-dd"),
-                                                toDate: utlatande.nedsattMed25.tomAsLocalDate().toString("yyyy-MM-dd")])
+                        // Insert base sjukfall cert data
+                        sql.execute("INSERT INTO SJUKFALL_CERT "
+                                + "(ID,CERTIFICATE_TYPE,CIVIC_REGISTRATION_NUMBER,PATIENT_FIRST_NAME,PATIENT_LAST_NAME,CARE_UNIT_ID,CARE_UNIT_NAME,CARE_GIVER_ID,SIGNING_DOCTOR_ID,SIGNING_DOCTOR_NAME,DIAGNOSE_CODE,DELETED,SIGNING_DATETIME)"
+                                + "VALUES (:id,:type,:personnummer,:firstName,:lastName,:careUnitId,:careUnitName,:careGiverId,:doctorId,:doctorName,:diagnosKod,:deleted,:signingDateTime)"
+                                , [id: id, type : utlatande.typ, personnummer: personnummer, firstName : firstName, lastName : lastName ,
+                                   careUnitId : careUnitId, careUnitName : careUnitName, careGiverId : careGiverId, doctorId : doctorId,
+                                   doctorName : doctorName, diagnosKod : diagnosKod, deleted : deleted, signingDateTime : sqlSigningDateTime
+                        ])
+
+                        // Insert one item per nedsattning
+                        String insertSql = "INSERT INTO SJUKFALL_CERT_WORK_CAPACITY (CERTIFICATE_ID,CAPACITY_PERCENTAGE,FROM_DATE,TO_DATE) VALUES(:id,:nedsattningProcent,:fromDate,:toDate)";
+                        if (utlatande.nedsattMed25 != null) {
+                            sql.execute(insertSql, [id:id,
+                                                    nedsattningProcent: 25,
+                                                    fromDate: utlatande.nedsattMed25.fromAsLocalDate().toString("yyyy-MM-dd"),
+                                                    toDate: utlatande.nedsattMed25.tomAsLocalDate().toString("yyyy-MM-dd")])
+                        }
+                        if (utlatande.nedsattMed50 != null) {
+                            sql.execute(insertSql, [id:id,
+                                                    nedsattningProcent: 50,
+                                                    fromDate: utlatande.nedsattMed50.fromAsLocalDate().toString("yyyy-MM-dd"),
+                                                    toDate: utlatande.nedsattMed50.tomAsLocalDate().toString("yyyy-MM-dd")])
+                        }
+                        if (utlatande.nedsattMed75 != null) {
+                            sql.execute(insertSql, [id:id,
+                                                    nedsattningProcent: 75,
+                                                    fromDate: utlatande.nedsattMed75.fromAsLocalDate().toString("yyyy-MM-dd"),
+                                                    toDate: utlatande.nedsattMed75.tomAsLocalDate().toString("yyyy-MM-dd")])
+                        }
+                        if (utlatande.nedsattMed100 != null) {
+                            sql.execute(insertSql, [id:id,
+                                                    nedsattningProcent: 100,
+                                                    fromDate: utlatande.nedsattMed100.fromAsLocalDate().toString("yyyy-MM-dd"),
+                                                    toDate: utlatande.nedsattMed100.tomAsLocalDate().toString("yyyy-MM-dd")])
+                        }
+                    } else {
+                        // If SJUKFALL_CERT already exists for id, just update the DELETED state.
+                        sql.execute( 'UPDATE SJUKFALL_CERT sc SET sc.deleted = :deleted WHERE id = :id', [deleted : row.DELETED, id : id])
                     }
-                    if (utlatande.nedsattMed50 != null) {
-                        sql.execute(insertSql, [id:id,
-                                                nedsattningProcent: 50,
-                                                fromDate: utlatande.nedsattMed50.fromAsLocalDate().toString("yyyy-MM-dd"),
-                                                toDate: utlatande.nedsattMed50.tomAsLocalDate().toString("yyyy-MM-dd")])
-                    }
-                    if (utlatande.nedsattMed75 != null) {
-                        sql.execute(insertSql, [id:id,
-                                                nedsattningProcent: 75,
-                                                fromDate: utlatande.nedsattMed75.fromAsLocalDate().toString("yyyy-MM-dd"),
-                                                toDate: utlatande.nedsattMed75.tomAsLocalDate().toString("yyyy-MM-dd")])
-                    }
-                    if (utlatande.nedsattMed100 != null) {
-                        sql.execute(insertSql, [id:id,
-                                                nedsattningProcent: 100,
-                                                fromDate: utlatande.nedsattMed100.fromAsLocalDate().toString("yyyy-MM-dd"),
-                                                toDate: utlatande.nedsattMed100.tomAsLocalDate().toString("yyyy-MM-dd")])
-                    }
+
 
                 } catch (Exception e) {
 
