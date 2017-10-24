0999fa29af26bd2cba49019fe6ed10129973039e
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-10-24 16:48:33 +0200

    INTYG-4806: Översättning + validering av gamla SetCertificateStatus

--- web/src/main/java/se/inera/intyg/intygstjanst/web/integration/SetCertificateStatusResponderImpl.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/SetCertificateStatusResponderImpl.java
@@ -21,16 +21,17 @@ package se.inera.intyg.intygstjanst.web.integration;
 import org.apache.cxf.annotations.SchemaValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.w3.wsaddressing10.AttributedURIType;
-
 import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatus.rivtabp20.v1.SetCertificateStatusResponderInterface;
 import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatusresponder.v1.SetCertificateStatusRequestType;
 import se.inera.ifv.insuranceprocess.healthreporting.setcertificatestatusresponder.v1.SetCertificateStatusResponseType;
 import se.inera.intyg.common.schemas.insuranceprocess.healthreporting.utils.ResultOfCallUtil;
 import se.inera.intyg.common.support.integration.module.exception.InvalidCertificateException;
 import se.inera.intyg.common.support.model.CertificateState;
-import se.inera.intyg.schemas.contract.Personnummer;
+import se.inera.intyg.intygstjanst.web.exception.RecipientUnknownException;
 import se.inera.intyg.intygstjanst.web.service.CertificateService;
 import se.inera.intyg.intygstjanst.web.service.MonitoringLogService;
+import se.inera.intyg.intygstjanst.web.service.RecipientService;
+import se.inera.intyg.schemas.contract.Personnummer;
 
 /**
  * @author andreaskaltenbach
@@ -44,18 +45,28 @@ public class SetCertificateStatusResponderImpl implements SetCertificateStatusRe
     @Autowired
     private MonitoringLogService monitoringLogService;
 
+    @Autowired
+    private RecipientService recipientService;
+
     @Override
     public SetCertificateStatusResponseType setCertificateStatus(AttributedURIType logicalAddress,
             SetCertificateStatusRequestType request) {
 
         SetCertificateStatusResponseType response = new SetCertificateStatusResponseType();
 
+        String target = request.getTarget();
+        // We need to translate FK->FKASSA as Försäkringskassan are still sending statuses with FK as target
+        if ("FK".equals(target)) {
+            target = recipientService.getPrimaryRecipientFkassa().getId();
+        }
+
         try {
+            target = recipientService.getRecipient(target).getId();
             certificateService.setCertificateState(new Personnummer(request.getNationalIdentityNumber()), request.getCertificateId(),
-                    request.getTarget(), CertificateState.valueOf(request.getStatus().name()), request.getTimestamp());
+                    target, CertificateState.valueOf(request.getStatus().name()), request.getTimestamp());
             response.setResult(ResultOfCallUtil.okResult());
             monitoringLogService.logCertificateStatusChanged(request.getCertificateId(), request.getStatus().name());
-        } catch (InvalidCertificateException e) {
+        } catch (RecipientUnknownException | InvalidCertificateException e) {
             response.setResult(ResultOfCallUtil.failResult(e.getMessage()));
         }
 
