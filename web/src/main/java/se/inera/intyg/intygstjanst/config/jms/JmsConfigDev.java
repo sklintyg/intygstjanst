5e6e7c652065dbdeb5bc044299c84793aac16782
Magnus Ekstrand <magnus@ekstrand.net>
2018-06-21 15:43:32 +0200

    OPENSHIFT: Lade till ApplicationConfig för at tläsa in properties etc

--- web/src/main/java/se/inera/intyg/intygstjanst/config/jms/JmsConfigDev.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/config/jms/JmsConfigDev.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.inera.intyg.intygstjanst.config;
+package se.inera.intyg.intygstjanst.config.jms;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
