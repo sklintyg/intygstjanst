0a10ed39a90a0c403d39f31baa85f9d98d64a80c
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-09-12 13:25:37 +0200

    INTYG-6999: Tog bort bortkommenterad kod

--- web/src/main/java/se/inera/intyg/intygstjanst/web/integration/util/CertificateStateFilterUtil.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/util/CertificateStateFilterUtil.java
@@ -18,10 +18,10 @@
  */
 package se.inera.intyg.intygstjanst.web.integration.util;
 
-import java.util.Objects;
-
 import se.inera.intyg.common.support.model.CertificateState;
 import se.inera.intyg.common.support.modules.support.api.CertificateStateHolder;
+
+import java.util.Objects;
 
 /**
  * Utility class containing business logic for what status removed for different consumers of the soap api.
@@ -41,8 +41,6 @@ public final class CertificateStateFilterUtil {
      *            the status to be either kept or removed
      * @param part
      *            the consumer of the api
-     * @param defaultRecipient
-     *            huvudmottagare of the intyg type the status item belongs to
      * @return whether to keep or the given status item
      */
     public static boolean filter(CertificateStateHolder status, String part) {
@@ -51,17 +49,13 @@ public final class CertificateStateFilterUtil {
             // Invanaren: alla statusar (INTYG-3629).
             return true;
         case "HSVARD":
-            // Varden: alla statusar förutom Arkiverat, Aterstallt, Skickat (om part inte är en
-            // huvudmottagare [default recipient]) (INTYG-3629)
+            // Varden: alla statusar förutom Arkiverat, Aterstallt
             if (status.getState() == CertificateState.DELETED || status.getState() == CertificateState.RESTORED) {
                 return false;
             }
-//            if (status.getState() == CertificateState.SENT && !Objects.equals(status.getTarget(), defaultRecipient)) {
-//                return false;
-//            }
             return true;
         default:
-            // FKKASSA och ovriga parter
+            // FKASSA och ovriga parter
             if (status.getState() == CertificateState.DELETED || status.getState() == CertificateState.RESTORED) {
                 return false;
             }
