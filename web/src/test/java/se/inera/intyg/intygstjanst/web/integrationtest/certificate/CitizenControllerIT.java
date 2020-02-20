553afe3bda57b680be6af8c7d20dcd3d4d4a4e60
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-02-20 10:16:19 +0100

    INTYGFV-12736: Fixed Integrationtest. Use patient that is not flagged as testIndicator true.

--- web/src/test/java/se/inera/intyg/intygstjanst/web/integrationtest/certificate/CitizenControllerIT.java
+++ web/src/test/java/se/inera/intyg/intygstjanst/web/integrationtest/certificate/CitizenControllerIT.java
@@ -43,7 +43,7 @@ public class CitizenControllerIT extends BaseIntegrationTest {
 
     static final String PATH = "/inera-certificate/internalapi/citizens/certificates";
 
-    static final String PID = "201212121212";
+    static final String PID = "191212121212";
 
     @Before
     public void before() {
