75e9f3dc4a002e6056e78a96e99b2cbb0255066c
Marcus Johansson <marcus.johansson@soprasteria.com>
2020-08-28 16:12:59 +0200

    INTYGFV-13196: added javadoc

--- web/src/main/java/se/inera/intyg/intygstjanst/web/service/TypedCertificateService.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/service/TypedCertificateService.java
@@ -24,17 +24,60 @@ import se.inera.intyg.infra.certificate.dto.DiagnosedCertificate;
 import se.inera.intyg.infra.certificate.dto.SickLeaveCertificate;
 import se.inera.intyg.schemas.contract.Personnummer;
 
+/**
+ * Service to provide certificate data with more specialized information than included in base information.
+ * Used for places were the whole information set of a certificate is not wanted and/or when there is a need for a more generalized type of
+ * certificate than the common base information.
+ */
 public interface TypedCertificateService {
 
+    /**
+     * List certificates on unit(s) with diagnosis information
+     *
+     * @param units List of units the certificates are bound to
+     * @param certificateTypeList The specific type of certificates to get
+     * @param fromDate First signing date of selection
+     * @param toDate Last signing date of selection
+     * @return List of certificates with diagnosis information
+     */
     List<DiagnosedCertificate> listDiagnosedCertificatesForCareUnits(List<String> units, List<String> certificateTypeList,
         LocalDate fromDate, LocalDate toDate);
 
+    /**
+     * List certificates for person with diagnosis information
+     *
+     * @param personId Id of the person to get certificates for
+     * @param certificateTypeList The specific type of certificates to get
+     * @param fromDate First signing date of selection
+     * @param toDate Last signing date of selection
+     * @param units List of units the certificates are bound to
+     * @return List of certificates with diagnosis information
+     */
     List<DiagnosedCertificate> listDiagnosedCertificatesForPerson(Personnummer personId, List<String> certificateTypeList,
         LocalDate fromDate, LocalDate toDate, List<String> units);
 
+    /**
+     * List certificates for person with sickleave information
+     *
+     * @param personId Id of the person to get certificates for
+     * @param certificateTypeList The specific type of certificates to get
+     * @param fromDate First signing date of selection
+     * @param toDate Last signing date of selection
+     * @param units List of units the certificates are bound to
+     * @return List of certificates with sickleave information
+     */
     List<SickLeaveCertificate> listSickLeaveCertificatesForPerson(Personnummer personId, List<String> certificateTypeList,
         LocalDate fromDate, LocalDate toDate, List<String> units);
 
+    /**
+     * List doctors that have signed certificates on unit(s)
+     *
+     * @param units List of units the certificates are bound to
+     * @param certificateTypes The specific type of certificates to get
+     * @param fromDate First signing date of selection
+     * @param toDate Last signing date of selection
+     * @return List of certificates with diagnosis information
+     */
     List<String> listDoctorsForCareUnits(List<String> units, List<String> certificateTypes,
         LocalDate fromDate, LocalDate toDate);
 }
