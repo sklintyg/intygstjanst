59f9cccc540a5afc9a5a0e622c7f8444c2c4b750
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-03-27 09:34:19 +0200

    INTYGFV-15421: Implemented ListActiveSickLeaveController with DTO SickLeaveRequestDTO.

---
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/sickleave/ListActiveSickLeaveController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2023 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.intygstjanst.web.integration.sickleave;
+
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import se.inera.intyg.infra.monitoring.annotation.PrometheusTimeMethod;
+import se.inera.intyg.infra.sjukfall.dto.SjukfallEnhet;
+import se.inera.intyg.intygstjanst.web.service.SickLeavesForCareUnitService;
+import se.inera.intyg.intygstjanst.web.service.dto.SickLeaveRequestDTO;
+
+@Path("/sickleave")
+public class ListActiveSickLeaveController {
+
+    private final SickLeavesForCareUnitService sickLeavesForCareUnitService;
+
+    public ListActiveSickLeaveController(SickLeavesForCareUnitService sickLeavesForCareUnitService) {
+        this.sickLeavesForCareUnitService = sickLeavesForCareUnitService;
+    }
+
+    @PrometheusTimeMethod
+    @POST
+    @Path("/active")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public List<SjukfallEnhet> getActiveSickLeaves(@RequestBody SickLeaveRequestDTO sickLeaveRequestDTO) {
+        // wrappa i ett response med listan som content
+        return sickLeavesForCareUnitService.getActiveSickLeavesForCareUnit(sickLeaveRequestDTO);
+    }
+}
