909c8e49c4986b8e3e050c391ff92edc1dd44b8e
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-04-18 16:55:56 +0200

    INTYGFV-15513: Last finess.

--- web/src/main/java/se/inera/intyg/intygstjanst/web/integration/testability/TestabilityController.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/testability/TestabilityController.java
@@ -21,7 +21,6 @@ package se.inera.intyg.intygstjanst.web.integration.testability;
 import static se.inera.intyg.intygstjanst.web.service.impl.TestabilityServiceImpl.ALFA_MEDICINCENTRUM;
 import static se.inera.intyg.intygstjanst.web.service.impl.TestabilityServiceImpl.ALFA_MEDICINCENTRUM_INFEKTIONSMOTTAGNINGEN;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -30,9 +29,6 @@ import javax.ws.rs.core.Response;
 import se.inera.intyg.infra.monitoring.annotation.PrometheusTimeMethod;
 import se.inera.intyg.intygstjanst.web.service.TestabilityService;
 
-/**
- * Internal REST endpoint for managing test certificates.
- */
 @Path("/testability")
 public class TestabilityController {
 
@@ -48,7 +44,6 @@ public class TestabilityController {
     @PrometheusTimeMethod
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     @Path("/createDefault")
     public Response createDefaultTestData() {
         testabilityService.createDefaultTestData();
