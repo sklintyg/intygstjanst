8ff1aafeef3f122bd7b354c4fae153f308580123
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-05-24 14:17:32 +0200

    INTYGFV-15627: Added tests

--- web/src/main/java/se/inera/intyg/intygstjanst/web/integration/reko/SetRekoStatusToSickLeaveRequestDTO.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/integration/reko/SetRekoStatusToSickLeaveRequestDTO.java
@@ -1,5 +1,6 @@
 package se.inera.intyg.intygstjanst.web.integration.reko;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class SetRekoStatusToSickLeaveRequestDTO {
     String patientId;
     String status;
