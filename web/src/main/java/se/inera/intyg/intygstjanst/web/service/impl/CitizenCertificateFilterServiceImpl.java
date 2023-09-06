99cf6a068d131b8e1b31e16270983518ba8d1ee8
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-09-06 11:52:59 +0200

    INTYGFV-15831: Added shell for filter

--- web/src/main/java/se/inera/intyg/intygstjanst/web/service/impl/CitizenCertificateFilterServiceImpl.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/service/impl/CitizenCertificateFilterServiceImpl.java
@@ -1,6 +1,34 @@
 package se.inera.intyg.intygstjanst.web.service.impl;
 
+import se.inera.intyg.intygstjanst.web.integration.citizen.CitizenCertificateStatusTypeDTO;
 import se.inera.intyg.intygstjanst.web.service.CitizenCertificateFilterService;
 import se.inera.intyg.intygstjanst.web.service.dto.citizen.CitizenCertificateDTO;
 
 import java.util.List;
+
+public class CitizenCertificateFilterServiceImpl implements CitizenCertificateFilterService {
+    @Override
+    public boolean filterOnYears(CitizenCertificateDTO certificate, List<String> includedYears) {
+        final var signedYear = (certificate.getIssued());
+
+        return includedYears
+                .stream()
+                .anyMatch((year) -> year.equals(signedYear));
+    }
+
+    @Override
+    public boolean filterOnSentStatus(CitizenCertificateDTO certificate, List<CitizenCertificateStatusTypeDTO> statuses) {
+        final var includeSent = false;
+        final var includeNotSent = false;
+
+        if (includeSent) {
+            return certificate.getRecipient() != null && certificate.getRecipient().getSent() != null;
+        }
+
+        if (includeNotSent) {
+            return certificate.getRecipient() != null && certificate.getRecipient().getSent() == null;
+        }
+
+        return true;
+    }
+}
