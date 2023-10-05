31b0394a40cd19d44cd9484f1acff24d7ccaad46
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-10-05 11:37:01 +0200

    INTYGFV-15887: Removed to do

--- web/src/main/java/se/inera/intyg/intygstjanst/web/service/impl/SendCertificateServiceImpl.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/service/impl/SendCertificateServiceImpl.java
@@ -26,9 +26,6 @@ public class SendCertificateServiceImpl implements SendCertificateService {
     this.internalNotificationService = internalNotificationService;
   }
 
-  // TODO: Should we add some logging here? Thinking of the "Certificate sent" "Certificate already sent" in SendCertificateToRecipientResponderImpl
-
-
   @Override
   public SendStatus send(SendCertificateRequestDTO request)
       throws InvalidCertificateException, TestCertificateException, CertificateRevokedException, RecipientUnknownException {
