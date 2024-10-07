e7e94b1a15dc1d56f5b79f9fdf4235fb90a0b02c
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2024-10-07 19:10:12 +0200

    K1J-358: Rename RestClientConfig -> ITRestClientConfig due to naming conflict with infra (#219)

--- web/src/main/java/se/inera/intyg/intygstjanst/config/ITRestClientConfig.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/config/ITRestClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-public class RestClientConfig {
+public class ITRestClientConfig {
 
     @Value("${certificateservice.base.url}")
     private String baseUrl;
