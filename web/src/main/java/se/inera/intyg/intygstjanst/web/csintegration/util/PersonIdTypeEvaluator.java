724f3820ad9923b97da308cfa6a600eb5f628753
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-10-16 10:55:56 +0200

    K1J-421: Add missing licence header.

--- web/src/main/java/se/inera/intyg/intygstjanst/web/csintegration/util/PersonIdTypeEvaluator.java
+++ web/src/main/java/se/inera/intyg/intygstjanst/web/csintegration/util/PersonIdTypeEvaluator.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2024 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.inera.intyg.intygstjanst.web.csintegration.util;
 
 import java.util.Optional;
@@ -7,13 +25,13 @@ import se.inera.intyg.schemas.contract.Personnummer;
 
 public class PersonIdTypeEvaluator {
 
-  private PersonIdTypeEvaluator() {
-    throw new IllegalStateException("Utility class");
-  }
+    private PersonIdTypeEvaluator() {
+        throw new IllegalStateException("Utility class");
+    }
 
-  public static PersonIdTypeDTO getType(Personnummer personId) {
-    return SamordningsnummerValidator.isSamordningsNummer(
-        Optional.of(personId)) ? PersonIdTypeDTO.COORDINATION_NUMBER
-        : PersonIdTypeDTO.PERSONAL_IDENTITY_NUMBER;
-  }
+    public static PersonIdTypeDTO getType(Personnummer personId) {
+        return SamordningsnummerValidator.isSamordningsNummer(
+            Optional.of(personId)) ? PersonIdTypeDTO.COORDINATION_NUMBER
+            : PersonIdTypeDTO.PERSONAL_IDENTITY_NUMBER;
+    }
 }
