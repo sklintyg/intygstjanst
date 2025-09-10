cce8e0a57f4f4ff52d4a1479949750ad7b4196ce
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-09-10 09:12:50 +0200

    K1J-1344: Fix tests for CertificateToSickLeaveConverter

--- web/src/test/java/se/inera/intyg/intygstjanst/web/service/converter/CertificateToSickLeaveConverterTest.java
+++ web/src/test/java/se/inera/intyg/intygstjanst/web/service/converter/CertificateToSickLeaveConverterTest.java
@@ -1,15 +1,15 @@
 package se.inera.intyg.intygstjanst.web.service.converter;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import se.inera.intyg.common.support.facade.builder.CertificateBuilder;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
@@ -52,14 +52,166 @@ class CertificateToSickLeaveConverterTest {
     private static final String WC2_ID = "HALFTEN"; // 50
     private static final LocalDate WC2_FROM = LocalDate.of(2025, 1, 11);
     private static final LocalDate WC2_TO = LocalDate.of(2025, 1, 20);
-  public static final String CARE_GIVER_NAME = "CARE_GIVER_NAME";
+    public static final String CARE_GIVER_NAME = "CARE_GIVER_NAME";
+    private static final String QUESTION_DIAGNOSIS_ID = "6";
+    private static final String QUESTION_SYSSELSATTNING_ID = "28";
+    private static final String QUESTION_BEDOMNING_ID = "32";
 
-  @Test
-    void convert_shouldMapAllFields() {
-        final var converter = new CertificateToSickLeaveConverter();
+    private CertificateToSickLeaveConverter converter;
 
+    @BeforeEach
+    void setUp() {
+        this.converter = new CertificateToSickLeaveConverter();
+    }
+
+    @Test
+    void shouldConvertId() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(CERT_ID, result.getId());
+    }
+
+    @Test
+    void shouldConvertCareGiverId() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(CARE_GIVER_ID, result.getCareGiverId());
+    }
+
+    @Test
+    void shouldConvertCareUnitId() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(CARE_UNIT_ID, result.getCareUnitId());
+    }
+
+    @Test
+    void shouldConvertCareUnitName() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(CARE_UNIT_NAME, result.getCareUnitName());
+    }
+
+    @Test
+    void shouldConvertType() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(CERT_TYPE_EXPECTED, result.getType());
+    }
+
+    @Test
+    void shouldConvertCivicRegistrationNumber() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(PATIENT_PERSON_ID, result.getCivicRegistrationNumber());
+    }
+
+    @Test
+    void shouldConvertSigningDoctorName() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(DOCTOR_FULL_NAME, result.getSigningDoctorName());
+    }
+
+    @Test
+    void shouldConvertPatientName() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(PATIENT_NAME, result.getPatientName());
+    }
+
+    @Test
+    void shouldConvertDiagnoseCode() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(DIAG_MAIN, result.getDiagnoseCode());
+    }
+
+    @Test
+    void shouldConvertBiDiagnoseCode1() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(DIAG_BI_1, result.getBiDiagnoseCode1());
+    }
+
+    @Test
+    void shouldConvertBiDiagnoseCode2() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(DIAG_BI_2, result.getBiDiagnoseCode2());
+    }
+
+    @Test
+    void shouldConvertSigningDoctorId() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(DOCTOR_PERSON_ID, result.getSigningDoctorId());
+    }
+
+    @Test
+    void shouldConvertSigningDateTime() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(SIGNED_AT, result.getSigningDateTime());
+    }
+
+    @Test
+    void shouldConvertDeleted() {
+        final var result = converter.convert(buildCertificate());
+        assertFalse(result.getDeleted());
+    }
+
+    @Test
+    void shouldConvertEmployment() {
+        final var result = converter.convert(buildCertificate());
+        assertEquals(EMPLOYMENT_1 + ", " + EMPLOYMENT_2, result.getEmployment());
+    }
+
+    @Test
+    void shouldConvertTestCertificate() {
+        final var result = converter.convert(buildCertificate());
+        assertFalse(result.isTestCertificate());
+    }
+
+    @Test
+    void shouldConvertWorkCapacitySize() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(2, capacities.size());
+    }
+
+    @Test
+    void shouldConvertFirstCapacityPercentage() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(100, capacities.getFirst().getCapacityPercentage());
+    }
+
+    @Test
+    void shouldConvertFirstCapacityFromDate() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(WC1_FROM.toString(), capacities.getFirst().getFromDate());
+    }
+
+    @Test
+    void shouldConvertFirstCapacityToDate() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(WC1_TO.toString(), capacities.getFirst().getToDate());
+    }
+
+    @Test
+    void shouldConvertSecondCapacityPercentage() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(50, capacities.get(1).getCapacityPercentage());
+    }
+
+    @Test
+    void shouldConvertSecondCapacityFromDate() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(WC2_FROM.toString(), capacities.get(1).getFromDate());
+    }
+
+    @Test
+    void shouldConvertSecondCapacityToDate() {
+        final var result = converter.convert(buildCertificate());
+        final var capacities = result.getSjukfallCertificateWorkCapacity();
+        assertEquals(WC2_TO.toString(), capacities.get(1).getToDate());
+    }
+
+    private static Certificate buildCertificate() {
         final Map<String, CertificateDataElement> certificateData = new HashMap<>();
-        certificateData.put("6", CertificateDataElement.builder()
+        certificateData.put(QUESTION_DIAGNOSIS_ID, CertificateDataElement.builder()
             .value(CertificateDataValueDiagnosisList.builder()
                 .list(
                     List.of(
@@ -69,7 +221,7 @@ class CertificateToSickLeaveConverterTest {
                 .build())
             .build()
         );
-        certificateData.put("28", CertificateDataElement.builder()
+        certificateData.put(QUESTION_SYSSELSATTNING_ID, CertificateDataElement.builder()
             .value(CertificateDataValueCodeList.builder()
                 .list(
                     List.of(
@@ -78,7 +230,7 @@ class CertificateToSickLeaveConverterTest {
                 .build())
             .build()
         );
-        certificateData.put("32", CertificateDataElement.builder()
+        certificateData.put(QUESTION_BEDOMNING_ID, CertificateDataElement.builder()
             .value(CertificateDataValueDateRangeList.builder()
                 .list(
                     List.of(
@@ -89,72 +241,42 @@ class CertificateToSickLeaveConverterTest {
         );
 
         final var careProvider = Unit.builder()
-          .unitId(CARE_GIVER_ID)
-          .unitName(CARE_GIVER_NAME)
-          .build();
+            .unitId(CARE_GIVER_ID)
+            .unitName(CARE_GIVER_NAME)
+            .build();
 
         final var careUnit = Unit.builder()
-          .unitId(CARE_UNIT_ID)
-          .unitName(CARE_UNIT_NAME)
-          .build();
+            .unitId(CARE_UNIT_ID)
+            .unitName(CARE_UNIT_NAME)
+            .build();
 
         final var personId = PersonId.builder()
             .id(PATIENT_PERSON_ID)
             .build();
 
         final var patient = Patient.builder()
-          .personId(personId)
-          .fullName(PATIENT_NAME)
-          .build();
+            .personId(personId)
+            .fullName(PATIENT_NAME)
+            .build();
 
         final var staff = Staff.builder()
             .personId(DOCTOR_PERSON_ID)
             .fullName(DOCTOR_FULL_NAME)
             .build();
 
-      final var certificateMetaData = CertificateMetadata.builder()
-          .id(CERT_ID)
-          .type(CERT_TYPE_INPUT)
-          .careProvider(careProvider)
-          .careUnit(careUnit)
-          .patient(patient)
-          .issuedBy(staff)
-          .signed(SIGNED_AT)
-          .build();
+        final var certificateMetaData = CertificateMetadata.builder()
+            .id(CERT_ID)
+            .type(CERT_TYPE_INPUT)
+            .careProvider(careProvider)
+            .careUnit(careUnit)
+            .patient(patient)
+            .issuedBy(staff)
+            .signed(SIGNED_AT)
+            .build();
 
-        // Build certificate
         final Certificate certificate = CertificateBuilder.create().build();
         certificate.setMetadata(certificateMetaData);
         certificate.setData(certificateData);
-
-        final var result = converter.convert(certificate);
-
-        assertEquals(CERT_ID, result.getId());
-        assertEquals(CARE_GIVER_ID, result.getCareGiverId());
-        assertEquals(CARE_UNIT_ID, result.getCareUnitId());
-        assertEquals(CARE_UNIT_NAME, result.getCareUnitName());
-        assertEquals(CERT_TYPE_EXPECTED, result.getType());
-        assertEquals(PATIENT_PERSON_ID, result.getCivicRegistrationNumber());
-        assertEquals(DOCTOR_FULL_NAME, result.getSigningDoctorName());
-        assertEquals(PATIENT_NAME, result.getPatientName());
-        assertEquals(DIAG_MAIN, result.getDiagnoseCode());
-        assertEquals(DIAG_BI_1, result.getBiDiagnoseCode1());
-        assertEquals(DIAG_BI_2, result.getBiDiagnoseCode2());
-        assertEquals(DOCTOR_PERSON_ID, result.getSigningDoctorId());
-        assertEquals(SIGNED_AT, result.getSigningDateTime());
-        assertFalse(result.getDeleted());
-        assertEquals(EMPLOYMENT_1 + ", " + EMPLOYMENT_2, result.getEmployment());
-        assertFalse(result.isTestCertificate());
-
-        final var capacities = result.getSjukfallCertificateWorkCapacity();
-        assertEquals(2, capacities.size());
-
-        assertEquals(100, capacities.get(0).getCapacityPercentage());
-        assertEquals(WC1_FROM.toString(), capacities.get(0).getFromDate());
-        assertEquals(WC1_TO.toString(), capacities.get(0).getToDate());
-
-        assertEquals(50, capacities.get(1).getCapacityPercentage());
-        assertEquals(WC2_FROM.toString(), capacities.get(1).getFromDate());
-        assertEquals(WC2_TO.toString(), capacities.get(1).getToDate());
+        return certificate;
     }
 }
