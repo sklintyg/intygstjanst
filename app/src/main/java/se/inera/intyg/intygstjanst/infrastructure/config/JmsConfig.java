eb7670c254ca3ff3f3fe9d90785290a400e904fe
mheglind <mikael.heglind@soprasteria.com>
2026-06-18 10:29:31 +0200

    K1J-2213: Updated activemq setting to resolve queue blocks when send certificate fail

---
+++ app/src/main/java/se/inera/intyg/intygstjanst/infrastructure/config/JmsConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2026 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.intygstjanst.infrastructure.config;
+
+import jakarta.jms.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+
+@Configuration
+public class JmsConfig {
+
+  @Bean
+  public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
+      @Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory,
+      DefaultJmsListenerContainerFactoryConfigurer configurer) {
+    final var factory = new DefaultJmsListenerContainerFactory();
+    configurer.configure(factory, connectionFactory);
+    factory.setCacheLevel(DefaultMessageListenerContainer.CACHE_NONE);
+    return factory;
+  }
+}
